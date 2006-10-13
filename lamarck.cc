300b9244fe81ebfeb8fe15039201bdd6eef861c7
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2006-10-13 03:23:08 +0000

    Forgot to add these

---
+++ lamarck.cc
@@ -0,0 +1,273 @@
+#include "lamarck.h"
+
+#include <lamarck/convex_hull.h>
+#include <lamarck/one_point_hull.h>
+
+#include <limits.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stdexcept>
+
+using opt::NO_MINIMIZER;
+using opt::WANG_MINIMIZER;
+using opt::PHYSICAL_MINIMIZER;
+using opt::LINEAR_MINIMIZER;
+using opt::SA_MINIMIZER;
+
+namespace LaDa 
+{
+  Lamarck :: ~Lamarck()
+  {
+    if (convex_hull)
+      delete convex_hull;
+    convex_hull = NULL;
+    if ( minimizers.size() )
+    {
+      std::vector< opt::Minimize_Base<t_fitness> * > :: iterator i_minimizer = minimizers.begin();
+      std::vector< opt::Minimize_Base<t_fitness> * > :: iterator i_end = minimizers.end();
+      for( ; i_minimizer != i_end; ++i_minimizer )
+        delete *i_minimizer;
+    }
+  };
+
+  bool Lamarck :: Load(const std::string &_filename) 
+  {
+    filename = _filename;
+    TiXmlDocument doc( filename.c_str() );
+    
+    if  ( !doc.LoadFile() )
+    {
+      std::cerr << "error while opening input file " << filename << std::endl
+                << doc.ErrorDesc() << std::endl; 
+      return false;
+    }
+
+    TiXmlHandle docHandle( &doc );
+    if ( not Load ( docHandle ) )
+    {
+      std::cerr << "error while  loading Lamarck parameters from " << filename << std::endl
+                << " tinyxml: " << doc.ErrorDesc() << std::endl;
+      return false;
+    }
+
+    // completes cluster list with equivalent clusters
+    add_equivalent_clusters();
+    
+    read_CH();
+    
+    generate_functional(structure, &functional);
+    
+    // fitness pointers  -- note that functiona->variables will be set
+    // to Individual::variables at each evaluation
+    fitness.set_baseline( convex_hull );
+    fitness.set_quantity( &functional ); 
+    functional.destroy_variables(); 
+
+
+    return true;
+  }
+
+  bool Lamarck :: read_CH()
+  {
+    { // first, creates ch
+      if ( convex_hull )
+        delete convex_hull;
+      if ( is_one_point_hull )
+        convex_hull = new VA_CE :: One_Point_Hull();
+      else
+      {
+        convex_hull = new VA_CE :: Convex_Hull();
+        init_convex_hull();
+      }
+    }
+    { // then attempts to ch file
+      TiXmlDocument doc( xml_filename.c_str() );
+      
+      if  ( !doc.LoadFile() ) // no convex hull to read...
+        return false;
+    
+      TiXmlHandle handle( &doc );
+      TiXmlElement *child = handle.FirstChild( "LaDa" ).FirstChild( "ConvexHull" ).Element();
+      if ( not child )
+        return false;
+      
+  
+      if ( not convex_hull )
+      {
+        std::cerr << "Error while creating convex hull" << std::endl;
+        return false;
+      }
+      
+      return convex_hull->Load(child, *axes);
+    } // end of attempt to read ch file
+  }
+
+
+  // loads input from filename file
+  bool Lamarck :: Load( TiXmlHandle &handle )
+  {
+    TiXmlElement *child;
+    rVector3d vec;
+
+    // clusters, lattice, harmonics ....
+    if ( not Functional_Builder :: Load (handle ) )
+      return false;
+     
+    xml_filename = "convex_hull.xml";
+    child = handle.FirstChild( "LaDa" ).FirstChild( "Filename" ).Element();
+    if ( child and child->Attribute("xml") )
+      xml_filename = child->Attribute("xml");
+      
+    // reads structure from input
+    child = handle.FirstChild( "LaDa" ).FirstChild( "Structure" ).Element();
+    if ( not child )
+    {
+      std::cerr << "Could not find Structure in " << filename << std::endl;
+      return false;
+    }
+    structure.Load(child, *axes);
+
+    child = handle.FirstChild( "LaDa" ).FirstChild( "GA" ).FirstChild("OnePointHull").Element();
+    if ( child )
+      is_one_point_hull = true;
+
+    return true;
+  }
+
+  void Lamarck :: print_xml()
+  {
+    TiXmlDocument doc;
+    TiXmlElement *LaDa_node = new TiXmlElement("LaDa");
+    TiXmlElement *convex_hull_node = new TiXmlElement("ConvexHull");
+
+    doc.SetTabSize(1);
+    doc.LinkEndChild( new TiXmlDeclaration("1.0", "", "") );
+    
+    doc.LinkEndChild( LaDa_node );
+    LaDa_node->LinkEndChild( convex_hull_node ); 
+    convex_hull->print_xml( convex_hull_node, *axes );
+
+    doc.SaveFile(xml_filename.c_str());
+  }
+
+
+  // adds endpoints to convex hull -- cluster list should be complete
+  // at this point!!
+  // single site CEs only
+  void Lamarck :: init_convex_hull()
+  {
+    Ising_CE::Structure struc; 
+    FUNCTIONAL func;
+
+    // creates structure...
+    struc.cell = lattice->cell;
+    struc.atoms.clear();
+    struc.atoms.push_back( Ising_CE::Atom( lattice->atom_pos(0), -1.0 ) );
+
+    // and functional - note that there is no CS 
+    // (in fact CS may segfault here )
+    generate_functional(struc, &func);
+    
+
+    // adds first endpoint
+    {
+      *(func.begin()) = -1.0;
+      VA_CE::Breaking_Point bp(func.get_Obj1()->evaluate(), struc);
+      static_cast<VA_CE::Convex_Hull*>(convex_hull)->force_add( bp );
+    }
+
+    // adds second endpoint
+    {
+      *(func.begin()) = 1.0;
+      struc.atoms.clear();
+      struc.atoms.push_back( Ising_CE::Atom( lattice->atom_pos(0), 1.0 ) );
+      VA_CE::Breaking_Point bp(func.get_Obj1()->evaluate(), struc);
+      static_cast<VA_CE::Convex_Hull*>(convex_hull)->force_add( bp );
+    }
+
+    // clean-up
+    func.destroy_variables();
+    delete func.get_Obj1();
+    delete func.get_Obj2();
+  }
+
+
+
+  // adds a minimizer in list 
+  unsigned Lamarck :: add_minimizer( unsigned _type, unsigned _n)
+  {
+    opt::Minimize_Base<t_fitness> *minimizer;
+
+    switch( _type )
+    {
+      case NO_MINIMIZER: 
+        minimizer = new opt::Minimize_Base<t_fitness>;
+        break;
+      case WANG_MINIMIZER: 
+        minimizer = new opt::Minimize_Wang<t_fitness>;
+        break;
+      case PHYSICAL_MINIMIZER: 
+        minimizer = new opt::Minimize_Ssquared<t_fitness>;
+        break;
+      case SA_MINIMIZER: 
+        minimizer = new opt::Minimize_Linear<t_fitness>;
+        static_cast< opt::Minimize_Linear<t_fitness>* >(minimizer)->simulated_annealing = true;
+        static_cast< opt::Minimize_Linear<t_fitness>* >(minimizer)->max_calls = _n;
+        break;
+      case LINEAR_MINIMIZER: 
+        minimizer = new opt::Minimize_Linear<t_fitness>;
+        static_cast< opt::Minimize_Linear<t_fitness>* >(minimizer)->simulated_annealing = false;
+        static_cast< opt::Minimize_Linear<t_fitness>* >(minimizer)->max_calls = _n;
+        break;
+      default:
+        std::cerr << "Unknown minimizer type in LaDa :: MinimizerOp" << std::endl;
+        exit(-1);
+        break;
+    }
+
+    minimizers.push_back( minimizer );
+    return minimizers.size() - 1;
+  };
+
+  bool Lamarck :: minimize( const t_individual &_indiv, const unsigned &_minimizer ) 
+  {
+    if ( _minimizer > minimizers.size() ) 
+      return false;
+
+    fitness.set_variables( _indiv.get_variables() );
+    return (minimizers[_minimizer])->minimize(fitness);
+  }
+  double Lamarck :: evaluate( t_individual &_indiv ) 
+  {
+    ++EvalCounter;
+    fitness.set_variables( _indiv.get_variables() );
+    double result = ( fitness.get_quantity() )->evaluate();
+    structure.set_atom_types( *_indiv.get_variables() );
+    if ( convex_hull->add_structure(result, structure) )
+      _indiv.invalidate_baseline();
+    return result;
+  }
+
+  void Lamarck :: print_xmgrace( std::ofstream &_f, bool _print_ch )
+  {
+    std::string special_char = ""; 
+    if ( not _print_ch ) 
+      special_char = "? ";
+    _f << "   evaluation calls: " << EvalCounter << std::endl;
+    _f << " # " << special_char << "polynomial calls: " 
+       << VA_CE::Polynome::nb_eval << " "
+       << VA_CE::Polynome::nb_eval_grad << " "
+       << VA_CE::Polynome::nb_eval_with_grad << std::endl;
+    _f << " # " << special_char << "bad guess " << opt::Minimize_Linear<FITNESS> :: bad_guess 
+       << "   good guess " << opt::Minimize_Linear<FITNESS> :: good_guess 
+       << std::endl
+       << " # " << special_char << "poleval calls " << opt::Minimize_Linear<FITNESS> :: nb_evals
+       << "   polgrad calls " << opt::Minimize_Linear<FITNESS> :: nb_grad_evals
+       << std::endl;
+ 
+    if( _print_ch )
+      convex_hull->print_out(_f, VA_CE::Convex_Hull::PRINT_XMGRACE);
+  }
+  
+} // namespace LaDa
---
+++ lamarck.h
@@ -0,0 +1,86 @@
+#ifndef _LADA_H_
+#define _LADA_H_
+
+//  MotU: Master of the Universe class
+//  controls everything, does everything, will cook your breakfast
+
+#include "individual.h"
+
+#include <lamarck/functional_builder.h>
+#include <lamarck/ch_template.h>
+
+#include <opt/fitness_function.h>
+#include <opt/opt_minimize.h>
+
+
+namespace LaDa
+{
+  typedef LaDa::Individual<> t_individual ; 
+  typedef opt::Fitness_Function<VA_CE::t_functional, VA_CE::CH_Template> t_fitness;
+  typedef VA_CE :: CH_Template t_convex_hull;
+  class Lamarck : public VA_CE :: Functional_Builder
+  {
+    public: 
+      unsigned EvalCounter;
+
+    protected:
+      typedef t_fitness FITNESS;
+
+    private:
+      Ising_CE::Structure structure;
+      FUNCTIONAL functional;
+      t_fitness fitness;
+      t_convex_hull *convex_hull;
+      std::string filename;
+      std::string xml_filename;
+      bool is_one_point_hull;
+      std::vector< opt::Minimize_Base<t_fitness> *> minimizers;
+
+
+    public:
+      Lamarck() : Functional_Builder(), EvalCounter(0), convex_hull(NULL), filename("input.xml"),
+                  is_one_point_hull(false), minimizers() 
+      {
+        minimizers.reserve(5);
+      }
+      virtual ~Lamarck();
+      bool Load(const std::string &_filename);
+      void print_xml();
+      void print_xmgrace();
+
+      double evaluate( const double &_x ) // convex hull only
+        { return convex_hull->evaluate(_x); }
+      double evaluate( t_individual & _individual );
+      bool minimize( const t_individual & _individual, const unsigned &_minimizer );
+    
+      t_fitness& get_functional (const t_individual &_individual) 
+        { return fitness; }
+      
+      t_convex_hull& get_convex_hull ()
+        { return *convex_hull; }
+
+      unsigned add_minimizer( unsigned type, unsigned n);
+
+      unsigned get_pb_size () const
+        { return structure.atoms.size(); }
+
+      void write_xmgrace_header( std::ofstream &_f )
+      {
+        if ( is_one_point_hull )
+          _f << "# One Point Convex Hull " << std::endl;
+        else
+          _f << "# N-Point Convex Hull " << std::endl;
+      }
+      void print_xmgrace( std::ofstream &_f, bool _print_ch );
+
+    protected:
+      virtual bool Load( TiXmlHandle &handle );
+      bool read_CH();
+      void run_debug();
+
+      void init_convex_hull();
+  };
+
+} // namespace LaDa
+
+#endif // _LADA_H_
