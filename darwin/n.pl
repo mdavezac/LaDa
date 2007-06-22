068218f86664666f027d94e8b3d3c2e59cc26553
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2007-06-22 17:43:45 +0000

    Adding scripts...

---
+++ darwin/n.pl
@@ -0,0 +1,271 @@
+#!/usr/bin/perl
+
+my %params;
+open IN, "COMMANDS";
+$params{'agr'}{'filename'}      =  <IN>;
+$params{'directory'}{'result'}  =  <IN>;
+$params{'GA string'}            =  <IN>;
+$params{'method string'}        =  <IN>;
+$params{'breeding string'}      =  <IN>;
+$params{'taboos string'}        =  <IN>;
+$params{'terminator string'}    =  <IN>;
+$params{'other string'}         =  <IN>;
+close IN;
+
+$params{"home"} = `cd; pwd`; chomp $params{"home"};
+$params{'iaga call'}           =  "lada >> out";
+$params{'max GA iters'} = 400;
+
+$params{'GA'}{'maxgen'} = 0;
+$params{'GA'}{'popsize'} = 100;
+$params{'GA'}{'rate'} = 0.1;
+
+$params{'method'}{'type'} = "optimum";
+$params{'method'}{'target'} = "0";
+$params{'method'}{'delta'} = "0";
+
+$params{'breeding'} = "";
+
+$params{'taboos'} = "";
+
+$params{'other'} = "";
+
+$params{'terminator'} = 1;
+
+
+
+
+if ( $params{'GA string'} =~ /maxgen:(\d+)/ )
+  { $params{'GA'}{'maxgen'} = $1; }
+if ( $params{'GA string'} =~ /popsize:(\d+)/ )
+  { $params{'GA'}{'popsize'} = $1; }
+if ( $params{'GA string'} =~ /rate:(\S+)/ )
+  { $params{'GA'}{'rate'} = $1; }
+
+if ( $params{'method string'} =~ /optimum/ )
+  { $params{'method'}{'type'} = "optimum"; }
+elsif ( $params{'method string'} =~ /BestOf:(\S+)/ )
+{
+  $params{'method'}{'type'} =  "BestOf";
+  $params{'method'}{'delta'} = $1;
+}
+elsif ( $params{'method string'} =~ /target:(\S+)/ )
+{
+  $params{'method'}{'type'} = "Target";
+  $params{'method'}{'target'} = $1;
+  if( $params{'method'}{'string'} !~ /delta:(\S+)/ )
+    { print " Forgot to include delta in method target \n"; exit; }
+  $params{'method'}{'delta'} = $1;
+}
+elsif ( $params{'method string'} =~ /CH/ )
+  { $params{'method'}{'type'} = "CH"; }
+else
+  { print "Couldn't determine method on input!!\n"; exit; }
+
+
+$params{'breeding'} .= " UtterRandom" if ( $params{'breeding string'} =~ /UtterRandom/ );
+$params{'breeding'} .= " Crossover"   if ( $params{'breeding string'} =~ /Crossover/ );
+$params{'breeding'} .= " Krossover"   if ( $params{'breeding string'} =~ /Krossover/ );
+$params{'breeding'} .= " SA" if ( $params{'breeding string'} =~ /SA/ );
+$params{'breeding'} .= " VA" if ( $params{'breeding string'} =~ /VA/ );
+$params{'breeding'} .= " Beratan" if ( $params{'breeding string'} =~ /Beratan/ );
+  
+$params{'taboos'} .= " pop" if ( $params{'taboos string'} =~ /pop/ );
+$params{'taboos'} .= " history" if ( $params{'taboos string'} =~ /history/ );
+
+if ( $params{'terminator string'} =~ /(\d+)/ )
+  { $params{'terminator'} = $1; }
+
+$params{'other'} = " history " if ( $params{'other string'} =~ /history/ );
+$params{'restart'} = " results " if ( $params{'other string'} =~ /true/ );
+
+$params{'ssc'} = 0;
+$params{'nb atoms'} = 20;
+if ( $params{'agr'}{'filename'} =~ /atoms:(\d+)_(\d+)_(\d+)/ )
+{
+  $params{'nb atoms'} = $1*$2*$3;
+  $params{'x'} = $1;
+  $params{'y'} = $2;
+  $params{'z'} = $3;
+  $params{'agr'}{'filename'} =~ s/atoms:(\d+)_(\d+)_(\d+)(\_|)//;
+  $params{'agr'}{'filename'} =~ s/^\s+//;
+  $params{'agr'}{'filename'} =~ s/\s+$//;
+}
+
+
+$params{'agr'}{'filename'} = "";
+$params{'agr'}{'filename'} = "true_" if ($params{'restart'} =~ /results/ );
+$params{'agr'}{'filename'} .= sprintf "atoms:%i_%i_%i_", $params{'x'}, $params{'y'}, $params{'z'};
+$params{'agr'}{'filename'} .= $params{'method'}{'type'};
+$params{'agr'}{'filename'} .= sprintf ":%.2f_%.2f", $params{'target'}, $params{'delta'}
+  if( $params{'method'}{'type'} =~ /target/ );
+$params{'agr'}{'filename'} .= sprintf ":%.2f", $params{'delta'} if( $params{'method'}{'delta'} =~ /BestOf/ );
+
+$params{'breeding'} =~ s/ /_/g;
+$params{'agr'}{'filename'} .= $params{'breeding'};
+$params{'breeding'} =~ s/_/ /;
+
+$params{'agr'}{'filename'} .= sprintf "_pop:%i", $params{'GA'}{'popsize'}
+    if ( $params{'GA'}{'popsize'} != 100 );
+$params{'agr'}{'filename'} .= sprintf "_rep:%.2f", $params{'GA'}{'rate'}
+    if ( $params{'GA'}{'rate'} != 0.1 );
+$params{'agr'}{'filename'} .= "_partition"
+    if ( $params{'GA string'} =~ /partition/ );
+
+$params{'xml'}{"filename"} = $params{'agr'}{'filename'};
+$params{'agr'}{"filename"} .= ".agr";
+$params{'xml'}{"filename"} .= ".xml";
+
+print $params{'agr'}{"filename"}, "\n"; 
+
+# begin work
+my %structure;
+
+my $cosa = 0;
+#system "rm -f $params{'agr'}{'filename'}";
+read_structure(); # data passes from PIfile to %structure hash
+launch_iaga(); 
+
+
+exit;
+
+
+sub launch_iaga()
+{
+  for( my $count =0; $count < $params{'max GA iters'}; $count++)
+  {
+    write_lamarck_input();
+    system "$params{'iaga call'}";
+    system "cat convex_hull.agr >> $params{'agr'}{'filename'}";
+    system "cp convex_hull.xml $params{'xml'}{'filename'}  ";
+    system "cp $params{'xml'}{'filename'} $params{'agr'}{'filename'}  $params{'directory'}{'result'} ";
+  }
+}
+
+sub read_structure()
+{
+  my $n = $params{'nb atoms'}; 
+  @{$structure{'cell'}} = ( [ 0.0, 0.5*$params{'x'}, 0.5*$params{'x'} ],
+                            [ 0.5*$params{'y'}, 0.0, 0.5*$params{'y'} ],
+                            [ 0.5*$params{'z'}, 0.5*$params{'z'}, 0.0 ] );
+  @{$structure{'atomic positions'}}  = ();
+  
+  my $index = 0;
+  for($i=0;$i<$params{'x'};$i++)
+  { 
+    for($j=0;$j<$params{'y'};$j++)
+    {
+      for($k=0;$k<$params{'z'};$k++)
+      {
+        $structure{'atomic positions'}[$index] = 0.5*($j+$k);
+        $index++;
+        $structure{'atomic positions'}[$index] = 0.5*($i+$k);
+        $index++;
+        $structure{'atomic positions'}[$index] = 0.5*($i+$j);
+        $index++;
+      }
+    }
+  }
+
+}
+
+
+sub write_lamarck_input()
+{
+  open IN, "<input_template.xml"
+    or die "No input_template.xml file found";
+  open OUT, ">input.xml"
+    or die "Could not open input.xml";
+
+  while( <IN> )
+  {
+
+    if( /\?structure\?/ )
+    {
+      printf OUT "  <Structure>\n";
+      printf OUT "    <Cell>\n";
+      printf OUT "      <column x=\"%.6f\" y=\"%.6f\" z=\"%.6f\" />\n",
+                 @{$structure{'cell'}[0]}[0..2];
+      printf OUT "      <column x=\"%.6f\" y=\"%.6f\" z=\"%.6f\" />\n",
+                 @{$structure{'cell'}[1]}[0..2];
+      printf OUT "      <column x=\"%.6f\" y=\"%.6f\" z=\"%.6f\" />\n",
+                 @{$structure{'cell'}[2]}[0..2];
+      printf OUT "    </Cell>\n";
+
+      for( my $i = 0; $i < scalar( @{$structure{'atomic positions'}} ); 
+           $i +=3 )
+      {
+        printf OUT "    <Atom x=\"%.6f\" y=\"%.6f\" z=\"%.6f\" type=\"%.6f\" />\n",
+                 $structure{'atomic positions'}[$i], 
+                 $structure{'atomic positions'}[$i+1], 
+                 $structure{'atomic positions'}[$i+2]; 
+      }
+      printf OUT "  </Structure>\n";
+    }
+    elsif( /\?GA\?/ )
+    {
+      printf OUT "  <GA ";
+      printf OUT "maxgen=\"%i\" " if( $params{'GA'}{'maxgen'} != 0 );
+      printf OUT "populate=\"partition\" " if( $params{'GA string'} =~ /partition/ );
+      printf OUT "popsize=\"%i\" rate=\"%.2f\">\n",
+                 $params{'GA'}{'popsize'}, 
+                 $params{'GA'}{'rate'};
+      printf OUT "    <Method type=\"%s\" ", $params{'method'}{'type'};
+      printf OUT "target=\"%.2f\" ", $params{'method'}{'target'}
+        if( $params{'method'}{'type'} =~ /target/ );
+      printf OUT "delta=\"%.2f\" ", $params{'method'}{'delta'}
+        if( $params{'method'}{'type'} =~ /(target|Best Of|BO)/ );
+      printf OUT " />\n";
+      
+      printf OUT "    <Breeding>\n";
+      if ( $params{'taboos'} =~ /(pop|history)/ )
+      {
+        printf OUT "      <TabooOp>\n";
+        printf OUT "        <UtterRandom/>\n" if ( $params{"breeding"} =~ /UtterRandom/ );
+        printf OUT "        <Krossover type=\"range\" />\n" if ( $params{"breeding"} =~ /Krossover/ );
+        printf OUT "        <Crossover/>\n" if ( $params{"breeding"} =~ /Crossover/ );
+        printf OUT "      </TabooOp>\n";
+      }
+      else
+      { 
+        printf OUT "      <UtterRandom/>\n" if ( $params{"breeding"} =~ /UtterRandom/ );
+        printf OUT "      <Krossover/>\n" if ( $params{"breeding"} =~ /Krossover/ );
+        printf OUT "      <Crossover/>\n" if ( $params{"breeding"} =~ /Crossover/ );
+      }
+      printf OUT "      <Minimizer type=\"VA\" />\n" if ( $params{"breeding"} =~ /VA/ );
+      printf OUT "      <Minimizer type=\"SA\" />\n" if ( $params{"breeding"} =~ /SA/ );
+      printf OUT "      <Minimizer type=\"Beratan\" />\n" if ( $params{"breeding"} =~ /Beratan/ );
+      printf OUT "    </Breeding>\n";
+
+
+      if( $params{'taboos'} =~ /(pop|history)/ )
+      {
+        printf OUT "    <Taboos>\n"; 
+        printf OUT "      <PopTaboo/>\n"  if ( $params{'taboos'} =~ /pop/ );  
+        printf OUT "      <OffspringTaboo/>\n" if ( $params{'taboos'} =~ /pop/ );   
+        printf OUT "      <HistoryTaboo/>\n" if ( $params{'taboos'} =~ /history/ );   
+        printf OUT "    </Taboos>\n"; 
+      }
+
+      printf OUT "    <History/>\n" if(    $params{'other'} =~ /history/ 
+                                        or $params{'taboos'} =~ /history/ );
+
+
+      printf OUT "    <Terminator ref=\"evaluation\" value=%i/>\n", $params{'terminator'}; 
+      printf OUT "    <Save what=\"all\"/>\n";
+      printf OUT "    <Restart what=\"results\"/>\n" if ( $params{'restart'} =~ /results/ );
+      printf OUT "    <Filenames restart=\"restart.xml\" />\n";
+
+      printf OUT "  </GA>\n";
+    }
+    else
+    {
+      print OUT $_;
+    }
+  }
+  
+
+  close IN;
+  close OUT;
+}
+
---
+++ darwin/send_all.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+# 1.  run type (multistart|lamarck + true )
+# 2.  miniizer type (none|sa|linear|wang|ssquare)
+# 3.  CH (one point|all points)
+# 4.  max calls
+# 5.  pop size
+# 6.  replacement rate
+# 7.  minimize best rate
+
+my @job = (#["popsize:20", "CH", "Krossover VA",         "pop", 400, "" ],
+#           ["popsize:60", "CH", "Krossover VA",         "pop", 400, "" ], 
+#           ["popsize:100","CH", "Krossover VA",         "pop", 400, "" ], 
+            ["popsize:20 rate:0.1", "CH", "Krossover",           "pop", 400, "" ],
+            ["popsize:20 rate:0.2", "CH", "Krossover",           "pop", 400, "" ],
+            ["popsize:20 rate:0.4", "CH", "Krossover",           "pop", 400, "" ],
+            ["popsize:20 rate:0.6", "CH", "Krossover",           "pop", 400, "" ],
+            ["popsize:20 rate:0.8", "CH", "Krossover",           "pop", 400, "" ] );
+#           ["popsize:60", "CH", "Krossover",            "pop", 400, "" ], 
+#           ["popsize:100","CH", "Krossover",            "pop", 400, "" ], 
+#           ["popsize:1 rate=1",  "CH", "UtterRandom VA",       "",    400, "" ],
+#           ["popsize:1 rate=1",  "CH", "UtterRandom Beratan",  "",    400, "" ] );
+#           ["popsize:60", "CH", "Krossover VA",         "pop", 400, " true " ] );
+my @prefixes = ("atoms:2_2_3");
+# 
+
+foreach $prefix ( @prefixes )
+{
+  for( my $i = 0; $i < scalar(@job) ; $i++)
+  {
+    system "rm -f sendme_$prefix$i ";
+    open OUT, ">sendme_$prefix$i" or die;
+    my $work = "work_$i";
+system "mkdir $work";
+    
+    printf OUT "#! /bin/tcsh\n#\n";
+    printf OUT "#PBS -l nodes=1:ppn=1,walltime=24:00:0\n";
+    printf OUT "#PBS -q Std\n";
+#   printf OUT "#PBS -l ncpus=1,walltime=48:00:0\n";
+#   printf OUT "#PBS -q single\n";
+    printf OUT "#PBS -m n \n";
+    printf OUT "#PBS -e std.err\n";
+    printf OUT "#PBS -o std.out\n\n";
+    printf OUT "set WORK_DIR=(\$PBS_O_WORKDIR/$work)\n";
+    printf OUT "set LOAD_DIR=(\$PBS_O_WORKDIR)\n";
+    printf OUT "set RESULT_DIR=(\$PBS_O_WORKDIR/results)\n\n";
+    printf OUT "module load intel\n\n";
+    printf OUT "cd \"\$LOAD_DIR\"\n";
+    printf OUT "rm sendme_$prefix$i.*\n";
+    printf OUT "cp ../lada ../n.pl input_template.xml \"\$WORK_DIR\"\n\n";
+    if ( $job[$i][0] =~ /local/ )
+    {
+      $_ = $prefixes[$i]; /atoms:(\d+)_(\d+)_(\d+)/;
+      my $chname = sprintf "true_ch_%i_%i_%i.xml", $1, $2, $3;
+      printf OUT "cp $chname \"\$WORK_DIR\"\n";
+    }
+    printf OUT "cd \"\$WORK_DIR\"\n\n";
+    printf OUT "cat >COMMANDS<<EOF\n";
+    printf OUT "%s\n",$prefix;
+    printf OUT "\$RESULT_DIR\n";
+    printf OUT "%s\n%s\n%s\n%s\n%s\n%s\n",
+  	 $job[$i][0], $job[$i][1], $job[$i][2], $job[$i][3],
+  	 $job[$i][4], $job[$i][5];
+    printf OUT "EOF\n";
+    printf OUT "chmod u+x n.pl\n\n";
+    printf OUT "n.pl > status\n\n";
+    printf OUT "exit\n";
+  
+    close OUT;
+  
+    system "chmode u+x sendme_$prefix$i";
+  
+    system "qsub sendme_$prefix$i > $prefix$i\_present_job"; 
+  
+  } 
+  
+  system "chmod u-x send_all.pl";
+
+}
