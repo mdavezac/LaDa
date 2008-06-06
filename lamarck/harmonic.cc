75c0e973317c6837b537b9784b063991a0451e9c
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2008-06-06 21:02:05 +0000

    Tetragonal CS checks out against Zhe's version, both lamarck//tetra and python bindings.

--- lamarck/harmonic.cc
+++ lamarck/harmonic.cc
@@ -25,6 +25,7 @@ namespace Ising_CE
       namespace details
       {
         inline types::t_real pow2( types::t_real _t ) { return _t*_t; }
+        inline types::t_real pow3( types::t_real _t ) { return _t * pow2( _t ); }
         inline types::t_real pow4( types::t_real _t ) { return pow2( pow2( _t ) ); }
         inline types::t_real pow6( types::t_real _t ) { return pow2( _t * pow2( _t ) ); }
         inline types::t_real pow8( types::t_real _t ) { return pow2( pow2( pow2( _t ) ) ); }
@@ -179,69 +180,59 @@ namespace Ising_CE
         using types::t_real;
         if( rank == 0 ) return t_real(1);
         types::t_real ctheta(1), cfai(1);
-        types::t_real stheta(1), sfai(1);
+        types::t_real stheta(0), sfai(0);
         if(    Fuzzy::neq(_k[0], t_real(0) )
             or Fuzzy::neq(_k[1], t_real(0) ) )
         {
-          ctheta =  _k[2] / atat::norm( _k );
-          stheta =  (_k[0] + _k[1]) / atat::norm( _k );
-          cfai   = _k[0] / std::sqrt( _k[0] * _k[0] + _k[1] * _k[1] );
-          sfai   = _k[1] / std::sqrt( _k[0] * _k[0] + _k[1] * _k[1] );
+          ctheta =  details::pow2(_k[2]) / atat::norm2( _k );
+          stheta =  t_real(1) - ctheta;
+          types::t_real a = _k[0] * _k[0] + _k[1] * _k[1]; 
+          cfai   = details::pow2( _k[0] ) / a;
+          sfai   = details::pow2( _k[1] ) / a; 
         }
-        if( rank == 1 ) return t_real(1.5) * details::pow2( ctheta ) - t_real(1);
+        if( rank == 1 ) return t_real(1.5) * ctheta - t_real(0.5);
         if( rank == 2 )
-          return   (   t_real(35) * details::pow4( ctheta )
-                     - t_real(30) * details::pow2( ctheta ) + t_real(3) ) 
+          return   (   t_real(35) * details::pow2( ctheta )
+                     - t_real(30) * ctheta + t_real(3) ) 
                  * t_real( 0.125 );
         if( rank == 3 )
-        {
-          types::t_real a = details::pow2(cfai);
-          types::t_real b = details::pow2(sfai);
-          return details::pow4( stheta ) * ( a - b - t_real(4) * a * b );
-        }
+          return   ( details::pow2(cfai - sfai) - t_real(4) * cfai * sfai ) 
+                 * details::pow2( stheta ) ;
         if( rank == 4 )
           return ( 
-                    t_real(231) * details::pow6(ctheta)
-                  - t_real(315) * details::pow4(ctheta) 
-                  + t_real(105) * details::pow2(ctheta)
+                    t_real(231) * details::pow3(ctheta)
+                  - t_real(315) * details::pow2(ctheta) 
+                  + t_real(105) * ctheta
                   - t_real(5)
                  ) * t_real( 0.0625 );
         if( rank == 5 )
         {
-          t_real a = details::pow2(cfai);
-          t_real b = details::pow2(sfai);
-          return   ( t_real(11) * details::pow2(ctheta) - t_real(1) )
-                 * ( a - b - t_real(4) * a * b )
-                 * details::pow4( stheta ) 
+          return   ( t_real(11) * ctheta - t_real(1) )
+                 * ( details::pow2(cfai - sfai) - t_real(4) * cfai * sfai )
+                 * details::pow2( stheta ) 
                  * t_real(0.81675);
         }
         if( rank == 6 )
           return (   
-                     t_real( 6435) * details::pow8( ctheta )
-                   - t_real(12012) * details::pow6( ctheta ) 
-                   + t_real( 6930) * details::pow4( ctheta ) 
-                   - t_real( 1260) * details::pow2( ctheta ) 
+                     t_real( 6435) * details::pow4( ctheta )
+                   - t_real(12012) * details::pow3( ctheta ) 
+                   + t_real( 6930) * details::pow2( ctheta ) 
+                   - t_real( 1260) * ctheta 
                    + t_real(   35) 
                  ) * std::sqrt( t_real(17) ) / t_real( 128 );
         if( rank == 7 )
-        {
-          t_real a = details::pow2(cfai);
-          t_real b = details::pow2(sfai);
-          return   ( a - b - t_real(4) * a * b )
-                 * details::pow4(stheta) 
+          return   ( details::pow2(cfai - sfai) - t_real(4) * cfai * sfai )
+                 * details::pow2(stheta) 
                  * t_real(3) / t_real(64) * std::sqrt( t_real( 654.5 ) )
-                 * (   t_real(65) * details::pow4(ctheta) 
-                     - t_real(26) * details::pow2(ctheta)
+                 * (   t_real(65) * details::pow2(ctheta) 
+                     - t_real(26) * ctheta
                      + t_real(1) );
-        }
         if( rank == 8 )
         {
-          t_real a = details::pow2(cfai);
-          t_real b = details::pow2(sfai);
-          t_real c = t_real(1) - t_real(8) * a * b; c *= c; 
-          t_real d = a - b; d *= d * t_real(8) * a * b;
+          t_real c = t_real(1) - t_real(8) * cfai * sfai; c *= c; 
+          t_real d = cfai - sfai; d *= d * t_real(8) * cfai * sfai;
           return   ( c - d ) // cos 8fai
-                 * details::pow8( stheta ) 
+                 * details::pow4( stheta ) 
                  * t_real(3) / t_real(128) * std::sqrt( t_real(6077.5) );
         }
                    
