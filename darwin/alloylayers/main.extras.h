fd070110c2198045ec78b9c897d915c5efdaf845
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2008-11-21 05:39:08 +0000

    props are correctly left aligned, by hand.

--- darwin/alloylayers/main.extras.h
+++ darwin/alloylayers/main.extras.h
@@ -82,17 +82,17 @@
 //   ga.evaluator.do_dipole( false );
     LaDa::Factory::Factory<void(void), std::string> properties_factory;
     properties_factory.connect
-      ( "epi", "Epitaxial Strain  in eV per f.u.\n"
-        "          (trace of strain - strain along growth direction).",
+      ( "epi", "   Epitaxial Strain  in eV per f.u.\n"
+        "             (trace of strain - strain along growth direction).",
         boost::bind<void>( &PPFactory::epitaxial_strain<t_Evaluator>, boost::ref(ga.evaluator) ) )
       ( "energy", "VFF energy in eV per f.u." ,
         boost::bind<void>( &PPFactory::strain_energy<t_Evaluator>, boost::ref(ga.evaluator) ) )
       ( "bandgap", boost::bind( &PPFactory::bandgap<t_Evaluator>, boost::ref(ga.evaluator) ) )
 //     ( "transition", "Dipole oscillator strength between VBM and CBM. Arbitrary units.",
 //       boost::bind( &PPFactory::transitions<t_Evaluator>, boost::ref(ga.evaluator) ) )
-      ( "cbm", "Conduction band minimum in eV.", 
+      ( "cbm", "   Conduction band minimum in eV.", 
         boost::bind( &PPFactory::cbm<t_Evaluator>, boost::ref(ga.evaluator) ) )
-      ( "vbm", "Valence band minimum in eV.",
+      ( "vbm", "   Valence band minimum in eV.",
         boost::bind( &PPFactory::vbm<t_Evaluator>, boost::ref(ga.evaluator) ) );
 
     // connects ga operator factory.
@@ -100,36 +100,38 @@
       t_OpFactory;
     t_OpFactory op_factory;
     op_factory.connect_attribute
-      ( "period", "operators with this attribute will be\n"
-        "             called only every n=\"period\" generations.\n",
+      ( "period", "    operators with this attribute will be\n"
+        "                 called only every n=\"period\" generations.\n",
          boost::bind( &LaDa::GA::Factory::periodic< t_OpFactory >,
                        _1, _2, _3, ga.get_counter() ) );
     op_factory.connect
-      ( "Operators", "container of operators. Accepts attribute "
-                     " type=\"and\" or type=\"or\".",
+      ( "Operators", " container of operators.\n"
+        "                 Accepts attribute type=\"and\" or type=\"or\".",
         boost::bind( &LaDa::GA::Factory::containers<t_OpFactory>, _1, _2, _3 ) ) 
-      ( "And", "Sequential container. same as <Operators type=\"and\"> ... <Operators/>.",
+      ( "And", "       Sequential container.\n"
+        "                 same as <Operators type=\"and\"> ... <Operators/>.",
         boost::bind( &LaDa::GA::Factory::sequential<t_OpFactory>, _1, _2, _3 ) ) 
-      ( "Or", "Proportional container. same as <Operators type=\"or\"> ... <Operators/>.",
+      ( "Or", "        Proportional container.\n"
+        "                 same as <Operators type=\"or\"> ... <Operators/>.",
         boost::bind( &LaDa::GA::Factory::sequential<t_OpFactory>, _1, _2, _3 ) )
-      ( "TabooOp", "An offspring created by the operators within this one\n"
-        "              will be rejected if it is in the tabooed individuals \n"
-        "              defined in <Taboos> .. </Taboos>. Offspring creation\n"
-        "              will repeat for at most n (attribute max=\"n\") trials,\n"
-        "              at which point a random individual is created.",
+      ( "TabooOp", "   An offspring created by the operators within this one\n"
+        "                 will be rejected if it is in the tabooed individuals \n"
+        "                 defined in <Taboos> .. </Taboos>. Offspring creation\n"
+        "                 will repeat for at most n (attribute max=\"n\") trials,\n"
+        "                 at which point a random individual is created.",
         boost::bind( &LaDa::GA::Factory::taboo_op<t_OpFactory, t_Darwin>, 
                      _1, _2, _3, boost::ref(ga), "Random" ) )
-      ( "Random", "creates a random individual.",
+      ( "Random", "    creates a random individual.",
         boost::bind( &PPFactory::random<t_OpFactory, t_Evaluator>,
                      _1, _2, _3, boost::ref(ga.evaluator) ) )
-      ( "Crossover", "bitstring-like crossover.",
+      ( "Crossover", " bitstring-like crossover.",
         boost::bind( &PPFactory::crossover<t_OpFactory>, _1, _2, _3 ) )
-      ( "Mutation", "bistring-like mutation.",
+      ( "Mutation", "  bistring-like mutation.",
         boost::bind( &PPFactory::mutation<t_OpFactory>, _1, _2, _3 ) );
     ga.set_operator_factory( op_factory );
-    if( print_genotype ) PPFactory::genotype( ga.evaluator );
-    PPFactory::read_physical_properties( properties_factory, input );
 
+
+    //
     LaDa::Factory::Factory< void(const std::string& ), std::string >& att_factory = ga.att_factory;
     ga.tournament_size = 2;
     ga.replacement_rate = 0.5;
@@ -138,12 +140,12 @@
     att_factory.connect
       (
         "tournament", "size of the deterministic tournaments\n"
-        "           for each parent selection. Default = 2.",
+        "                 for each parent selection. Default = 2.",
         bl::var(ga.tournament_size) = bl::bind( &call_lexcast<LaDa::types::t_unsigned>, bl::_1 )
       )
       (
         "rate", "      replacement rate (eg offspring creation rate).\n"
-        "           Default = 0.5.",
+        "                 Default = 0.5.",
         bl::var(ga.replacement_rate) = bl::bind( &call_lexcast<LaDa::types::t_real>, bl::_1 ) 
       )
       (
@@ -151,26 +153,27 @@
         bl::var(ga.pop_size) = bl::bind( &call_lexcast<LaDa::types::t_unsigned>, bl::_1 ) 
       )
       (
-        "maxgen", "    Max number of generations before quitting."
-        "           Default = 0.",
+        "maxgen", "    Max number of generations before quitting.\n"
+        "                 Default = 0.",
         bl::var(ga.max_generations) = bl::bind( &call_lexcast<LaDa::types::t_unsigned>, bl::_1 ) 
       )
       (
-        "islands", "    Number of independent islands. Default = 1.",
+        "islands", "   Number of independent islands. Default = 1.",
         bl::var(ga.nb_islands) = bl::bind( &call_lexcast<LaDa::types::t_unsigned>, bl::_1 ) 
       )
       (
-        "seed", "       random seed. Default = 0."
-        "            In MPI, seed0, seed1, ... are accepted,"
-        "            if not necessarily used.",
+        "seed", "      random seed. Default = 0.\n"
+        "                 In MPI, seed0, seed1, ... are accepted,\n"
+        "                 if not necessarily used.",
         boost::bind( &LaDa::GA::Topology::seed_n, ga.topology, 0, _1 )
       );
 #    ifdef _MPI
         for( size_t ii(0); ii < LaDa::mpi::main->size(); ++ii )
           att_factory.connect
           (
-            "seed" + boost::lexical_cast<std::string>(ii),
-            "       random seed. Default = 0.",
+               "seed" + boost::lexical_cast<std::string>(ii),
+               "     random seed for process " + boost::lexical_cast<std::string>(ii) 
+               + ". Default = 0.",
             boost::bind( &LaDa::GA::Topology::seed_n, ga.topology, 0, _1 )
           );
 #     endif
@@ -179,10 +182,13 @@
     // reads program options.
 #   undef _MAIN_ALLOY_LAYERS_EXTRAS_
 #   define _MAIN_ALLOY_LAYERS_EXTRAS_ 5
+    if( print_genotype ) PPFactory::genotype( ga.evaluator );
+    PPFactory::read_physical_properties( properties_factory, input );
+
 # else 
     // makes sure this file cannot be (meaningfully) included anymore.
 #   undef _MAIN_ALLOY_LAYERS_EXTRAS_
-#   define _MAIN_ALLOY_LAYERS_EXTRAS_
+#   define _MAIN_ALLOY_LAYERS_EXTRAS_ nofurtherinclude
 # endif
 
 #endif
