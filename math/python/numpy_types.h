028fea21a70b0faf4846be98aa7993e82ee78297
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2010-02-02 11:01:05 -0700

    numpy_types.h seems to work. Moving it to different dir

--- math/python/numpy_types.h
+++ math/python/numpy_types.h
@@ -36,7 +36,7 @@ namespace LaDa
       template<> struct type<npy_longlong> : public boost::mpl::int_<NPY_LONGLONG> 
       {
         //! Original type.
-        typedef npu_longlong np_type;
+        typedef npy_longlong np_type;
       };
       //! numpy identifier for unsigned long.
       template<> struct type<npy_ulonglong> : public boost::mpl::int_<NPY_ULONGLONG> 
@@ -92,59 +92,58 @@ namespace LaDa
         //! Original type.
         typedef npy_ubyte np_type;
       };
-      //! numpy identifier for bool.
-      template<> struct type<npy_bool> : public boost::mpl::int_<NPY_BOOL> 
-      {
-        //! Original type.
-        typedef npy_ubyte np_type;
-      };
+//     //! numpy identifier for bool.
+//     template<> struct type<npy_bool> : public boost::mpl::int_<NPY_BOOL> 
+//     {
+//       //! Original type.
+//       typedef npy_bool np_type;
+//     };
       
       //! Returns true if object is float.
-      bool is_float(PyObject *_obj_ptr)
+      inline bool is_float(PyObject *_obj_ptr)
       {
         int const nptype = PyArray_ObjectType(_obj_ptr, 0);
-        return    npy_type ==  NPY_FLOAT
-               or npy_type ==  NPY_DOUBLE
-               or npy_type ==  NPY_LONGDOUBLE;
+        return    nptype ==  NPY_FLOAT
+               or nptype ==  NPY_DOUBLE
+               or nptype ==  NPY_LONGDOUBLE;
       }
       
       //! Returns true if object is integer.
-      bool is_integer(PyObject *_obj_ptr)
+      inline bool is_integer(PyObject *_obj_ptr)
       {
         int const nptype = PyArray_ObjectType(_obj_ptr, 0);
-        return    npy_type ==  NPY_INT
-               or npy_type ==  NPY_UINT
-               or npy_type ==  NPY_LONG
-               or npy_type ==  NPY_ULONG
-               or npy_type ==  NPY_LONGLONG
-               or npy_type ==  NPY_ULONGLONG
-               or npy_type ==  NPY_BYTE
-               or npy_type ==  NPY_UBYTE
-               or npy_type ==  NPY_SHORT
-               or npy_type ==  NPY_USHORT
+        return    nptype ==  NPY_INT
+               or nptype ==  NPY_UINT
+               or nptype ==  NPY_LONG
+               or nptype ==  NPY_ULONG
+               or nptype ==  NPY_LONGLONG
+               or nptype ==  NPY_ULONGLONG
+               or nptype ==  NPY_BYTE
+               or nptype ==  NPY_UBYTE
+               or nptype ==  NPY_SHORT
+               or nptype ==  NPY_USHORT;
       }
       //! Returns true if object is boolean.
-      bool is_integer(PyObject *_obj_ptr) { return PyArray_ObjectType(_obj_ptr, 0) ==  NPY_BOOL; }
+      inline bool is_bool(PyObject *_obj_ptr) { return PyArray_ObjectType(_obj_ptr, 0) ==  NPY_BOOL; }
       
       //! Returns true if downcasting from PyObject to T. 
       template<class T> bool is_downcasting(PyObject *_obj_ptr) 
       {
-        int const nptype = PyArray_ObjectType(_obj_ptr, 0);
-        switch(nptype)
+        switch(PyArray_ObjectType(_obj_ptr, 0))
         {
-          case NPY_FLOAT     : return sizeof(type<NPY_FLOAT>::np_type) > sizeof(T);
-          case NPY_DOUBLE    : return sizeof(type<NPY_DOUBLE>::np_type) > sizeof(T);
-          case NPY_LONGDOUBLE: return sizeof(type<NPY_LONGDOUBLE>::np_type) > sizeof(T);
-          case NPY_INT       : return sizeof(type<NPY_INT>::np_type) > sizeof(T);
-          case NPY_UINT      : return sizeof(type<NPY_UINT>::np_type) > sizeof(T);
-          case NPY_LONG      : return sizeof(type<NPY_LONG>::np_type) > sizeof(T);
-          case NPY_ULONG     : return sizeof(type<NPY_ULONG>::np_type) > sizeof(T);
-          case NPY_LONGLONG  : return sizeof(type<NPY_LONGLONG>::np_type) > sizeof(T);
-          case NPY_ULONGLONG : return sizeof(type<NPY_ULONGLONG>::np_type) > sizeof(T);
-          case NPY_BYTE      : return sizeof(type<NPY_BYTE>::np_type) > sizeof(T);
-          case NPY_UBYTE     : return sizeof(type<NPY_UBYTE>::np_type) > sizeof(T);
-          case NPY_SHORT     : return sizeof(type<NPY_SHORT>::np_type) > sizeof(T);
-          case NPY_USHORT    : return sizeof(type<NPY_USHORT>::np_type) > sizeof(T);
+          case NPY_FLOAT     : return sizeof(type<npy_float>::np_type) > sizeof(T);
+          case NPY_DOUBLE    : return sizeof(type<npy_double>::np_type) > sizeof(T);
+          case NPY_LONGDOUBLE: return sizeof(type<npy_longdouble>::np_type) > sizeof(T);
+          case NPY_INT       : return sizeof(type<npy_int>::np_type) > sizeof(T);
+          case NPY_UINT      : return sizeof(type<npy_uint>::np_type) > sizeof(T);
+          case NPY_LONG      : return sizeof(type<npy_long>::np_type) > sizeof(T);
+          case NPY_ULONG     : return sizeof(type<npy_ulong>::np_type) > sizeof(T);
+          case NPY_LONGLONG  : return sizeof(type<npy_longlong>::np_type) > sizeof(T);
+          case NPY_ULONGLONG : return sizeof(type<npy_ulonglong>::np_type) > sizeof(T);
+          case NPY_BYTE      : return sizeof(type<npy_byte>::np_type) > sizeof(T);
+          case NPY_UBYTE     : return sizeof(type<npy_ubyte>::np_type) > sizeof(T);
+          case NPY_SHORT     : return sizeof(type<npy_short>::np_type) > sizeof(T);
+          case NPY_USHORT    : return sizeof(type<npy_ushort>::np_type) > sizeof(T);
           default: break;
         };
         throw;
