fd3ed57e6e1b957f7ea326dd5d5ceebb5b061207
Mayeul D'Avezac <mdavezac@head.(none)>
2010-02-09 16:21:08 -0700

    Adding python bindings to vasp-as-library

---
+++ vasp/vasp.cc
@@ -0,0 +1,21 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <boost/python/def.hpp>
+#include <boost/python/module.hpp>
+#include <boost/python/docstring_options.hpp>
+#include <boost/python/scope.hpp>
+
+extern "C" { FC_FUNC(vasplib, vasplib)(MPI_Comm *); }
+
+BOOST_PYTHON_MODULE(_vasp)
+{
+  namespace bp = boost::python;
+  bp::scope scope;
+  scope.attr("__doc__") = "VASP-as-library extension.";
+  bp::docstring_options doc_options(true, false);
+
+  bp::def( "vasp", &FC_FUNC(vasplib, vasplib),
+           "Make a call to vasp, given the handle to a communicator." );
+}
