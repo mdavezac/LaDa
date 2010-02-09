cf33f4aee5272e39b84055318678c4d408cc3677
Mayeul D'Avezac <mdavezac@head.(none)>
2010-02-08 18:34:00 -0700

    Rewrote Directness evaluator to account for vff/pescan api changes.
    Other evaluators are probably incorrect.

--- ga/pescan/elemental/evaluator.py
+++ ga/pescan/elemental/evaluator.py
@@ -16,9 +16,9 @@ class Bandgap(object):
         @type lattice: None or lada.crystal.Lattice
     """
     from boost.mpi import world
-    from lada.vff import LayeredVff
-    from lada.escan import BandGap as BGFunctional
-    from lada import crystal
+    from ....vff import LayeredVff
+    from ....escan import BandGap as BGFunctional
+    from ... import crystal
 
     self.world = mpi
     """ MPI Communicator. Defaults to boost.mpi.world. """
@@ -62,8 +62,8 @@ class Bandgap(object):
     from os.path import exists
     from os import makedirs
     from shutil import rmtree
-    from lada import crystal
-    from lada.opt.changedir import Changedir
+    from ... import crystal
+    from ....opt.changedir import Changedir
  
     # moves to new calculation directory
     self.bandgap.directory = self.directory_prefix + "_" + str(self.nbcalc)
@@ -141,7 +141,7 @@ class Dipole(Bandgap):
     return result
 
 
-class Directness(Bandgap):
+class Directness(object):
   """ Objective function for quasi-direct bandgaps. """
   X = np_array( [0,0,1], dtype="float64" )
   """ An X point of the reciprocal zinc-blend lattice. """
@@ -152,37 +152,74 @@ class Directness(Bandgap):
   W = np_array( [1, 0.5,0], dtype="float64" )
   """ A W point of the reciprocal zinc-blend lattice. """
 
-  def __init__(self, *args, **kwargs): 
+  def __init__( self, converter, which, input = "input.xml", lattice = None, mpi = None):
+    """ Initializes the Directness object. 
+
+        @param converter: is a functor which converts between a bitstring and a
+          lada.crystal.Structure, and vice versa.
+        @type  converter: duck-typed to L{Converter}
+        @param which: Each element in this sequence contains a k-point, the
+          name of that k-point, and a callable returning the reference energy
+          as a function of the structure. The first kpoint the one we want the
+          lowest.
+        @type which: sequence of 3-tuples
+        @param input: is the XML input file for the vff, pescan, 
+          (optionally) lattice parameters.
+        @type input: string
+        @param lattice: Zinc-Blende lattice. If not given, it must be in the
+          input file.
+        @type lattice: None or lada.crystal.Lattice
+    """
     from copy import deepcopy
-    self.which = (self.G, "Gamma")
-    if "which" in kwargs.keys():
-      self.which = deepcopy(kwargs["which"])
-      del kwargs["which"]
+    from numpy.linalg import norm
+    from ....vff import LayeredVff
+    from ....escan import Escan
 
-    super(Directness, self).__init__(*args, **kwargs)
+    self.converter = converter 
+    """ Conversion functor between structures and bitstrings. """
+    self.which = which
+    """ Parameters for computation. """
+    assert len(self.which) > 1 
+    self.directory_prefix = "indiv"
+    """ Directory prefix """
+    self.nbcalc = 0
 
-    
+    self.lattice = lattice
+    """ Zinc-blend lattice. """
+    if self.lattice == None: self.lattice = crystal.Lattice(input)
+    self.lattice.set_as_crystal_lattice()
+
+    self.world = mpi
+    """ MPI Communicator. Defaults to boost.mpi.world. """
+    if self.world == None: self.world = world
+
+    self.vff = LayeredVff(input, self.world) # Creates vff functional
+    """ Vff functional """
+    self.vff.direction = converter.structure.cell[:,0]
+
+    self.escan = Escan(input, self.world) # creates an escan functional
+    """ Bandgap functional """
+    self.escan.nbstates = 1 # only one state computed.
+
+
   def __call__(self, indiv):
-    """ Evaluates differences between kpoints. """
+    """ Evaluates directness of the gap """
     from os.path import exists, join
     from os import makedirs
     from shutil import rmtree
     from numpy import dot as np_dot, matrix as np_matrix
-    from lada.escan import Bands
-    from lada import crystal
-    from lada.opt.changedir import Changedir
+    from ....escan import Bands
+    from ....crystal import deform_kpoint
+    from ....opt.changedir import Changedir
 
     self.nbcalc += 1
     results = []
 
     # relax structure.
     structure = self.converter(indiv.genes)
-    relaxed = self.vff(structure)
+    relaxed, stress = self.vff(structure)
     indiv.epi_energy = relaxed.energy
-    # computes deformation of reciprocal lattice
-    deformation = np_dot(np_matrix(relaxed.cell).I.T, structure.cell.T)
-    # vff input file
-    self.bandgap.vff_inputfile = "atom_input." + str( self.world.rank )
+    indiv.epi_stress = stress
 
     # create and change directory.
     basedir = self.directory_prefix + "_" + str(self.nbcalc)
@@ -190,35 +227,36 @@ class Directness(Bandgap):
     if self.world.do_print:
       if exists(basedir): rmtree(basedir)
 
-    for kpoint, name in self.which:
+    for kpoint, name, reference in self.which:
       # create new calc directory
-      self.bandgap.directory = join(basedir, name)
-      if self.world.do_print: makedirs(self.bandgap.directory)
+      self.escan.directory = join(basedir, name)
+      if self.world.do_print: makedirs(self.escan.directory)
       self.world.barrier()
 
-      
       # change to calc directory
-      with Changedir(self.bandgap.directory) as pwd:
+      with Changedir(self.escan.directory) as pwd:
 
         # computes ideal folded kpoint.
-        self.bandgap.kpoint = crystal.fold_vector(kpoint, np_matrix(structure.cell).I.T)
-        # computes kpoint in deformed structure.
-        self.bandgap.kpoint = np_dot(deformation, self.bandgap.kpoint)
-        # prints bandgap input
-        self.vff.print_escan_input(self.bandgap.vff_inputfile)
-        #  computes bandgap.
-        self.bandgap(relaxed)
-        # saves bandgap result in individual
-        setattr(indiv, name, Bands(self.bandgap.bands) )
-        
-        results.append( Bands(self.bandgap.bands) )
+        self.escan.kpoint = deform_kpoint(kpoint, structure.cell, relaxed.cell)
+        # gets reference energy from input function.
+        self.escan.reference = reference(relaxed)
+        #  computes energy
+        eigenvalues = self.escan(self.vff, relaxed)
+        # saves eigenvalue result in individual
+        setattr(indiv, name, eigenvalues[0] )
+        # stores result.
+        results.append( eigenvalues[0] )
 
     # destroy directory if requested.
-    if self.bandgap.destroy_directory: rmtree(self.bandgap.directory)
+    if self.escan.destroy_directory: rmtree(self.escan.directory)
 
     # returns maximum offset from gamma 
-    result = results[0].cbm - results[1].cbm
-    if len(results) > 2: 
-      for bands in  results[2:]:
-        result = max( result, results[0].cbm - bands.cbm )
-    return result
+    gap = results[0] - results[1]
+    for e in  results[2:]:
+      gap = max( gap, results[0] - e )
+    return gap
+
+  def __len__(self):
+    """ Returns length of bitstring. """
+    return len(self.converter.structure.atoms)
+
