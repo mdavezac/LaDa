f863893bee208814733d226379defb3059e41c77
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2010-02-19 18:44:54 -0700

    changing bindings a bit to accomodate vasp4 and vasp5

--- vasp/vasp.cc
+++ vasp/vasp.cc
@@ -9,41 +9,69 @@
 #include <boost/python/module.hpp>
 #include <boost/python/docstring_options.hpp>
 #include <boost/python/scope.hpp>
+#include <boost/python/errors.hpp>
 
-extern "C" { void FC_FUNC(vasplib, VASPLIB)(MPI_Fint *); }
+namespace bp = boost::python;
+#ifdef LADA_EXPORT_VASP
+# error LADA_EXPORT_VASP already defined
+#endif
+#ifdef LADA_EXPORT_NOVASP
+# error LADA_EXPORT_NOVASP already defined
+#endif
+#define LADA_EXPORT_VASP(a)\
+  extern "C" { void FC_FUNC(vasplib ## a, VASPLIB ## a)(MPI_Fint *); }   \
+                                                                         \
+  void vasp ## a(MPI_Comm const * const _c)                              \
+  {                                                                      \
+    Py_BEGIN_ALLOW_THREADS;                                              \
+    MPI_Fint __commF = MPI_Comm_c2f( *_c );                              \
+    FC_FUNC(vasplib ## a, VASPLIB ## a)(&__commF);                       \
+    Py_END_ALLOW_THREADS;                                                \
+  }                                                                      \
+                                                                         \
+  void boost_vasp ## a(boost::mpi::communicator const &_c)               \
+  {                                                                      \
+    Py_BEGIN_ALLOW_THREADS;                                              \
+    MPI_Comm __commC = (MPI_Comm) ( _c ) ;                               \
+    MPI_Fint __commF = MPI_Comm_c2f( __commC );                          \
+    FC_FUNC(vasplib ## a, VASPLIB ## a)(&__commF);                       \
+    Py_END_ALLOW_THREADS;                                                \
+  }                                                                      \
+                                                                         \
+  void expose_vasp ## a(bp::scope &_scope)                               \
+  {                                                                      \
+    _scope.attr( "is_vasp" #a ) = a;                                     \
+    bp::def("vasp", &boost_vasp ## a, bp::arg("communicator"));          \
+    bp::def                                                              \
+    (                                                                    \
+      "vasp" #a,                                                         \
+      &vasp ## a,                                                        \
+      bp::arg("communicator"),                                           \
+      "Make a call to vasp, given the handle to a communicator.\n\n"     \
+      "@param communicator: the processes which vasp will use.\n"        \
+      "@type communicator: boost.mpi.Communicator or mpi4py equivalent"  \
+    );                                                                   \
+  }
 
-void vasp(MPI_Comm const * const _c) 
-{
-  Py_BEGIN_ALLOW_THREADS;
-  MPI_Fint __commF = MPI_Comm_c2f( *_c );
-  FC_FUNC(vasplib, VASPLIB)(&__commF);
-  Py_END_ALLOW_THREADS;
-}
+#define LADA_EXPORT_NOVASP(a) void expose_vasp ## a(bp::scope &)  {}                                 
 
-void vasp2(boost::mpi::communicator const &_c) 
-{ 
-  Py_BEGIN_ALLOW_THREADS;
-  MPI_Comm __commC = (MPI_Comm) ( _c ) ;
-  MPI_Fint __commF = MPI_Comm_c2f( __commC );
-  FC_FUNC(vasplib, VASPLIB)(&__commF);
-  Py_END_ALLOW_THREADS;
-}
+#ifdef LADA_VASP_FOUR
+  LADA_EXPORT_VASP(4)
+#else
+  LADA_EXPORT_NOVASP(4)
+#endif
+#ifdef LADA_VASP_FIVE
+  LADA_EXPORT_VASP(5)
+#else
+  LADA_EXPORT_NOVASP(5)
+#endif
+#undef LADA_EXPORT_VASP
 
 BOOST_PYTHON_MODULE(_vasp)
 {
-  namespace bp = boost::python;
   bp::scope scope;
   scope.attr("__doc__") = "VASP-as-library extension.";
   bp::docstring_options doc_options(true, false);
-
-  bp::def("vasp", &vasp2, bp::arg("communicator"));
-  bp::def
-  ( 
-    "vasp",
-    &FC_FUNC(vasplib, VASPLIB),
-    bp::arg("communicator"),
-    "Make a call to vasp, given the handle to a communicator.\n\n" 
-    "@param communicator: an mpi communicator. It can be either a boost.mpi, or "
-    "a mpi4py communicator."
-  );
+  expose_vasp4(scope);
+  expose_vasp5(scope);
 }
