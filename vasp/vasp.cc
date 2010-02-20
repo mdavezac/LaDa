5947b54e29e49ae2983b33645ec84b0d93a853c1
Mayeul d'Avezac <mdavezac@gmail.com>
2010-02-19 23:31:40 -0700

    redigned direct vasp call. Added version number.

--- vasp/vasp.cc
+++ vasp/vasp.cc
@@ -9,69 +9,57 @@
 #include <boost/python/module.hpp>
 #include <boost/python/docstring_options.hpp>
 #include <boost/python/scope.hpp>
+#include <boost/python/str.hpp>
 #include <boost/python/errors.hpp>
 
 namespace bp = boost::python;
-#ifdef LADA_EXPORT_VASP
-# error LADA_EXPORT_VASP already defined
-#endif
-#ifdef LADA_EXPORT_NOVASP
-# error LADA_EXPORT_NOVASP already defined
-#endif
-#define LADA_EXPORT_VASP(a)\
-  extern "C" { void FC_FUNC(vasplib ## a, VASPLIB ## a)(MPI_Fint *); }   \
-                                                                         \
-  void vasp ## a(MPI_Comm const * const _c)                              \
-  {                                                                      \
-    Py_BEGIN_ALLOW_THREADS;                                              \
-    MPI_Fint __commF = MPI_Comm_c2f( *_c );                              \
-    FC_FUNC(vasplib ## a, VASPLIB ## a)(&__commF);                       \
-    Py_END_ALLOW_THREADS;                                                \
-  }                                                                      \
-                                                                         \
-  void boost_vasp ## a(boost::mpi::communicator const &_c)               \
-  {                                                                      \
-    Py_BEGIN_ALLOW_THREADS;                                              \
-    MPI_Comm __commC = (MPI_Comm) ( _c ) ;                               \
-    MPI_Fint __commF = MPI_Comm_c2f( __commC );                          \
-    FC_FUNC(vasplib ## a, VASPLIB ## a)(&__commF);                       \
-    Py_END_ALLOW_THREADS;                                                \
-  }                                                                      \
-                                                                         \
-  void expose_vasp ## a(bp::scope &_scope)                               \
-  {                                                                      \
-    _scope.attr( "is_vasp" #a ) = a;                                     \
-    bp::def("vasp", &boost_vasp ## a, bp::arg("communicator"));          \
-    bp::def                                                              \
-    (                                                                    \
-      "vasp" #a,                                                         \
-      &vasp ## a,                                                        \
-      bp::arg("communicator"),                                           \
-      "Make a call to vasp, given the handle to a communicator.\n\n"     \
-      "@param communicator: the processes which vasp will use.\n"        \
-      "@type communicator: boost.mpi.Communicator or mpi4py equivalent"  \
-    );                                                                   \
-  }
-
-#define LADA_EXPORT_NOVASP(a) void expose_vasp ## a(bp::scope &)  {}                                 
-
-#ifdef LADA_VASP_FOUR
-  LADA_EXPORT_VASP(4)
-#else
-  LADA_EXPORT_NOVASP(4)
-#endif
-#ifdef LADA_VASP_FIVE
-  LADA_EXPORT_VASP(5)
-#else
-  LADA_EXPORT_NOVASP(5)
-#endif
-#undef LADA_EXPORT_VASP
+extern "C"
+{ 
+  void FC_FUNC(vasplib, VASPLIB)(MPI_Fint *); 
+  void FC_FUNC_(vasp_version, VASP_VERSION)(int *, int *, int *);
+}    
+                                                                        
+void vasp(MPI_Comm const * const _c)                               
+{                                                                       
+  Py_BEGIN_ALLOW_THREADS;                                               
+  MPI_Fint __commF = MPI_Comm_c2f( *_c );                               
+  FC_FUNC(vasplib, VASPLIB)(&__commF);                        
+  Py_END_ALLOW_THREADS;                                                 
+}                                                                       
+                                                                        
+void boost_vasp(boost::mpi::communicator const &_c)                
+{                                                                       
+  Py_BEGIN_ALLOW_THREADS;                                               
+  MPI_Comm __commC = (MPI_Comm) ( _c ) ;                                
+  MPI_Fint __commF = MPI_Comm_c2f( __commC );                           
+  FC_FUNC(vasplib, VASPLIB)(&__commF);                        
+  Py_END_ALLOW_THREADS;                                                 
+}                                                                       
+                                                                        
+void expose_vasp()
+{                                                                       
+  bp::def("vasp", &boost_vasp, bp::arg("communicator"));           
+  bp::def                                                               
+  (                                                                     
+    "vasp",                                                          
+    &vasp,                                                         
+    bp::arg("communicator"),                                            
+    "Make a call to vasp, given the handle to a communicator.\n\n"      
+    "POSCAR, INCAR, IBZKPT, and other input files are expected in the current "
+    "working directory.\n"
+    "@param communicator: the processes which vasp will use.\n"         
+    "@type communicator: boost.mpi.Communicator or mpi4py equivalent"   
+  );                                                                    
+}
 
 BOOST_PYTHON_MODULE(_vasp)
 {
   bp::scope scope;
   scope.attr("__doc__") = "VASP-as-library extension.";
   bp::docstring_options doc_options(true, false);
-  expose_vasp4(scope);
-  expose_vasp5(scope);
+  int minor=0, major=0, medium=0;
+  FC_FUNC_(vasp_version, VASP_VERSION)(&major, &medium, &minor);
+  std::ostringstream sstr; sstr << major << "." << medium << "." << minor;
+  scope.attr( "version" ) = sstr.str();
+  expose_vasp();
 }
