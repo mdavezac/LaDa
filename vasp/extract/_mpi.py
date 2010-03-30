d724ebe2ef4a95855492bcd6e29d77fa9597cb06
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2010-03-29 18:58:11 -0600

    adds caching to vasp.Extract

--- vasp/extract/_mpi.py
+++ vasp/extract/_mpi.py
@@ -8,7 +8,7 @@ def _bound_mpi_extraction(method, *args, **kwargs):
       Decorator for bound methods.
   """
   assert len(args) >= 1, "Decorator for bound methods only.\n"
-  return _extract_which_is_self(0, method, *args, **kwargs)
+  return _cache_method_result(0, method, *args, **kwargs)
   
 @decorator
 def _attribute_mpi_extraction(method, *args, **kwargs): 
@@ -17,7 +17,18 @@ def _attribute_mpi_extraction(method, *args, **kwargs):
       Decorator for properties.
   """
   assert len(args) >= 2, "Decorator for bound methods only.\n"
-  return _extract_which_is_self(1, method, *args, **kwargs)
+  return _cache_method_result(1, method, *args, **kwargs)
+
+def _cache_method_result(which, method, *args, **kwargs):
+  """ Saves grep result for later reuse """
+  cache_name = "_cached_attr%s" % (method.__name__)
+  if not hasattr(args[which], cache_name): 
+    setattr(args[which], cache_name, _extract_which_is_self(which, method, *args, **kwargs))
+  return getattr(args[which], cache_name)
+
+def _uncache(ob):
+  for key in ob.__dict__.keys():
+    if key[:len("_cached_attr")] == "_cached_attr": del ob.__dict__[key]
 
 def _extract_which_is_self(which, method, *args, **kwargs):
   from boost.mpi import broadcast
