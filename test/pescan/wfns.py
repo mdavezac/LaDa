ad6dd3fda7e3ffdc432aaa0b67d16d0d36d1134e
Mayeul d'Avezac <mdavezac@gmail.com>
2010-04-26 22:14:19 -0600

    Manual fft workds. trying to create general fft example.

--- test/pescan/wfns.py
+++ test/pescan/wfns.py
@@ -30,11 +30,11 @@ def create_structure():
   return Si, Ge
 
 from sys import exit
-from math import ceil, sqrt
+from math import ceil, sqrt, pi
 from os.path import join, exists
 from numpy import dot, array, matrix
 from numpy.linalg import norm
-from boost.mpi import world, all_reduce
+from boost.mpi import world, all_reduce, broadcast
 from lada.vff import Vff
 from lada.escan import Escan, method, nb_valence_states as nbstates, potential, Wavefunctions, to_realspace
 from lada.crystal import deform_kpoint
@@ -101,6 +101,13 @@ for (kpoint, name), structure, relaxed in [ (G, Si, Si_relaxed) ]:
       print wfns.shape, gvectors.shape, projs.shape
       rspace, rvectors = to_realspace(wfns, escan.comm)
       print rspace.shape, rvectors.shape
+      rvec = broadcast(world, rvectors[1], 0) 
+      gvec = broadcast(world, gvectors[1], 0)
+      if world.rank == 0: 
+        cell = matrix(Si_relaxed.cell* structure.scale /a0("A"))
+        kcell = cell.I.T
+        print gvec, np.dot(kcell, array([2e0*pi, 0, 0])) 
+        print rvec, (cell * matrix([0,0, 1e0/escan.genpot.mesh[0]]).T).T
       for i in range(wfns.shape[1]):
         a = np.matrix(rspace[:,i,0]) # np.multiply(wfns[:,i,j], projs))
         c = all_reduce(world, a*a.T.conjugate(), lambda x,y: x+y)
@@ -110,7 +117,40 @@ for (kpoint, name), structure, relaxed in [ (G, Si, Si_relaxed) ]:
         if world.rank == 0: print c * volume/s, volume
         u = 0e0 + 0e0*1j
         for k in range(wfns.shape[0]):
-          u += np.exp(- 1j *np.dot(rvectors[10,:], gvectors[k, :])) * wfns[k,i,0] * projs[k]
+          u += np.exp(-1j *np.dot(rvec, gvectors[k, :])) * wfns[k,i,0] # * projs[k]
         u = all_reduce(world, u, lambda x,y:x+y)
-        if world.rank == 0: print u, rspace[10,i,0]*s
+        if world.rank == 0: print u, rspace[1,i,0], u.real/rspace[1,i,0].real
 
+
+def gtor_fourrier(wavefunctions, rvectors, gvectors, comm):
+  import numpy as np
+  rshape = [ u for u in wavefunctions ]
+  rshape[0] = gvectors.shap[0]
+  result = np.zeros(rshape)
+
+  def iterate(axis, first, second):
+    assert first.ndims == second.ndims
+    for i in range(first.ndims):
+      if i == axis: 
+      assert first
+    if first.ndims == 1:
+      yield first, second
+      return
+    # makes axis the last dimension
+    if axis != first.ndims:
+      fswapped = np.swapaxes(first, axis, first.ndims-1)
+    # generator for iterating over all other dimensions.
+    def all_but_last(iterated):
+      if iterated.ndims == 1: yield iterated
+      for next_ in iterated: yield(next_)
+    yield all_but_last(swapped)
+
+
+
+
+    for node in range(comm.size):
+      for r in rvectors.shape[0]:
+        rvec = broadcast(world, rvectors[r], node)
+        v = zeros((gvectors.shape[0],), dtype="float64")
+        for g in range(gvectors.shape[0]): v[g] = np.exp(np.dot(rvec, gvectors[g])) 
+        result[r,:,:] = np.dot( 
