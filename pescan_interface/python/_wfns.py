8afe0ccdf4756ea7a5d68e745efdc50ad20134b8
Mayeul d'Avezac <mdavezac@gmail.com>
2010-05-11 22:29:25 -0600

    ?

--- pescan_interface/python/_wfns.py
+++ pescan_interface/python/_wfns.py
@@ -16,7 +16,25 @@ class Wavefunctions(Extract):
   @property
   @make_cached
   @broadcast_result(attr=True, which=0)
-  def _raw(self): pass
+  def _raw(self):
+    """ Reads and caches g-space wavefunction data. 
+    
+        This is a tuple described making up the return of
+        L{read_wavefunctions<lada.escan._escan.read_wavefunctions>}
+    """
+    from ._escan import read_wavefunctions
+
+    assert self.comm.size >= self.nnodes,\
+           RuntimeError("Must read wavefunctions with at least "\
+                        "as many nodes as they were written to disk.")
+    if self.comm.size > self.nnodes:
+      color = 0 if self.comm.rank < self.nnodes else 1
+      local_comm = self.comm.split(color)
+    else: color, local_comm = 0, self.comm
+    if color == 1: return (,,,)
+    return read_wavefunctions(self.escan, [i for i in range(self.nbstates)], local_comm)
+
+
 
   @property
   @make_cached
