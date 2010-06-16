bbea9cc244694ef8690ce64f0fe65b960df9d02f
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2010-06-16 17:45:34 -0600

    some changes to Neighbors python bindings

--- crystal/python/neighbors.cc
+++ crystal/python/neighbors.cc
@@ -62,7 +62,8 @@ namespace LaDa
       namespace bp = boost::python;
 
       typedef Crystal::Neighbor Neighbor;
-      bp::class_<Neighbor>("Neighbor", "holds information of nth neighbor.")
+      bp::class_<Neighbor>("Neighbor", "Describes nth neighbor.\n\n"
+                           "This is the object returned by L{Neighbors}.")
           .def(bp::init<Neighbor const&>())
           .add_property("index", &Neighbor::index, "Index of atom in structure.")
           .add_property("pos", &Neighbor::pos, "Position respect to the origin.")
@@ -71,10 +72,14 @@ namespace LaDa
       bp::class_<Neighbors>
       (
         "Neighbors", 
-        "Iterator over first neighbors.", 
-        bp::init<Crystal::TStructure<std::string> const &>()
-      ).def(bp::init<Crystal::TStructure<std::string> const &, size_t, math::rVector3d>())
-       .def("__iter__", &Neighbors::iter, bp::return_internal_reference<1>())
+        "Iterator over first neighbors.\n\n"
+        "Initialization:\n"
+        " - The first argument (L{Structure}) is the structure within which to find neighbors.\n"
+        " - The second argument (integer) is the number of neighbors to look for.\n"
+        " - The third argument (3x numpy array) is the central atom position "
+        "for which to find neightbors.",
+        bp::init<Crystal::TStructure<std::string> const &, size_t, math::rVector3d>()
+      ).def("__iter__", &Neighbors::iter, bp::return_internal_reference<1>())
        .def("next", &Neighbors::next, bp::return_internal_reference<1>());
     }
 
