f685123cbc95c9156a854466b48d02258d0128b9
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2010-06-28 13:11:06 -0700

    making pyvirt in templates automatic

--- jobs/templates.py
+++ jobs/templates.py
@@ -4,7 +4,7 @@
 """
 
 def default_pbs( file, walltime = "05:45:00", mppwidth = 8, queue = "regular", name = None, \
-                 pyvirt = "vasp.4.6.32", pools = 1,  pyscript = None, pickle = "job_pickle", \
+                 pyvirt = None, pools = 1,  pyscript = None, pickle = "job_pickle", \
                  outdir = None, python_path = None, **kwargs):
   """ Creates default pbs-script. Does not launch. 
 
@@ -14,19 +14,26 @@ def default_pbs( file, walltime = "05:45:00", mppwidth = 8, queue = "regular", n
       @param mppwidth: Number of processes (not processors) to use.
       @param queue: Queue to use
       @param name: Name of the job.
-      @param pyvirt: Virtual python environment. Does nothing if None.
+      @param pyvirt: Virtual python environment. If None, checks for
+        $VIRTUAL_ENV environment variable and uses that if it exists. Otherwise
+        does nothing.
       @param pools: Number of mpi pools (parallelization over processes).
       @param pyscript: Python script to launch. Default: L{runme.py}
       @param pythonpath: Additional path to search for modules.
       @param pickle: Fileame of job-tree pickle.
   """
-  from os.path import exists
+  from os import environ
+  from os.path import exists, split as splitpath
+
+  if pyvirt == None and "VIRTUAL_ENV" in environ:
+    pyvirt = splitpath(environ["VIRTUAL_ENV"])[1]
+
 
   file.write("#! /bin/bash\n")
   file.write("#PBS -l walltime=%s,mppwidth=%i\n" % (walltime, mppwidth) )
   file.write("#PBS -q " + queue + "\n")
-  file.write("#PBS -e err\n")
-  file.write("#PBS -o out\n")
+  file.write("#PBS -e err.$PBS_JOBID\n")
+  file.write("#PBS -o out.$PBS_JOBID\n")
   if name != None: file.write("#PBS -N %s \n\n" % (name))
 
   if outdir == None: file.write("cd $PBS_O_WORKDIR\n")
