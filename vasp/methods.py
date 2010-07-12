d5c21bffdbbc24ccdcc8036786321cd95267690a
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2010-07-12 11:28:28 -0600

    adding no run option to RelaxCellShape

--- vasp/methods.py
+++ vasp/methods.py
@@ -89,6 +89,10 @@ class RelaxCellShape(object):
     from os import getcwd
     from os.path import join, exists
 
+    # does not run code. Just creates directory.
+    if kwargs.pop("norun", False): 
+      yield self._norun(structure, outdir, comm, **kwargs)
+      return
 
     # make this function stateless.
     vasp = kwargs.pop("vasp", self.vasp)
@@ -163,126 +167,24 @@ class RelaxCellShape(object):
         If you want to examine the result of each and every vasp calculation,
         use `generator` instead.
     """ 
+    if kwargs.pop("norun", False): 
+      return self._norun(structure, outdir, comm, **kwargs)
+
     for output in self.generator(*args, **kwargs): pass
     return output
+  
+  def _norun(structure, outdir, comm, **kwargs):
+    """ Just creates directory for debugging. """
+    from ..opt.changedir import Changedir
 
+    is_root = True if comm == None else comm.rank == 0
+    if is_root:
+      # creates a file describing the relaxation parameters.
+      with Changedir(outdir) as pwd:
+        with open("relaxe_cell_shape_parameters") as file:
+          file.write( "self.relaxation = %s\n" % (repr(self.relaxation)) )
+          file.write( "self.maxiter = %s\n" % (repr(self.maxiter)) )
+          file.write( "self.first_trial = %s\n" % (repr(self.first_trial)) )
 
-# def kpoint_convergence(vasp, structure, outdir="kconv", comm = None, start=1, steps=None, \
-#                        offset=(0.5,0.5,0.5), repat=[], tolerance=1e-3, **kwargs):
-#   """ Performs a convergence test for kpoints using kpoints.Density object.
-
-#       This is a generator which yields a output extraction object after each
-#       vasp calculation:
-#       >>> for extract in kpoint_convergence(structure, vasp, outdir)
-#       >>>   print extract.total_energy
-#       Note that this function will not recompute the result if an appropriate
-#       output directory is found. 
-#       @note: This method works only if C{vasp.kpoints = integer(??)} is correct.
-#       @note: This functor is stateless as long as self and structure can be
-#              deepcopied correctly.  
-
-#       @param structure: A structure to relax.
-#       @type structure: L{lada.crystal.Structure} 
-#       @param vasp: The vasp functional.
-#       @type vasp: L{Vasp}
-#       @param outdir: Directory where to repatriate files. Default = kconv.
-#       @type outdir: str
-#       @param comm: Groups of processes for which to call vasp.
-#       @type comm: None or boost.mpi.Communicator 
-#       @param start: Starting density. Default = 1.
-#       @param steps: End density. Default = 1.
-#       @param offset: Offset from L{Gamma} of reciprocal mesh. Default = (0.5,0.5,0.5). 
-#       @type offset: 3-tuple.
-#       @param repat: File to repatriate, other than L{files.minimal}. Default: [].
-#       @type repat: list or set
-#       @param tolerance: Total energy convergence criteria (per atom). Default: 1e-3. 
-#       @type tolerance: float
-#   """
-#   from copy import deepcopy
-#   from math import fabs as abs
-#   from os.path import join, exists
-#   from os import remove
-#   from ..kpoints import Density
-#   from .. import files
-
-#   # make this function stateless.
-#   vasp = deepcopy(vasp)
-#   repat = set(repat).union(files.minimal)
-#   tolerance = float(tolerance) * float( len(structure.atoms) )
-#   vasp.relaxation.value = "static" # what else could it be?
-#   density = deepcopy(start)
-
-#   # keywords arguments cannot include kpoints.
-#   if "kpoints" in kwargs:
-#     raise SyntaxError,\
-#           "Cannot have kpoints as a keyword argument when performing kpoints convergence.\n"
-
-#   # we may want to include restart file to speed up calculations. However,
-#   # these files should be deleted afterwards.
-#   other_repat = []
-#   for file in files.restart: 
-#     if file not in repat: other_repat.append(file)
-#   repat.union(other_repat)
-
-#   # performs initial calculation.
-#   outdirs = ["%s/density:%i" % (outdir, density)]
-#   output = vasp( structure, outdirs[-1], comm = comm, repat = repat,\
-#                  kpoints=Density(offset, density), **kwargs )
-#   # yields output for whatnot
-#   yield output 
-#   # makes sure we don't accidentally converge to early.
-#   oldenergy = -output.total_energy
-
-#   while( abs(oldenergy - output.total_energy) > tolerance ):
-#     # restart + new directory
-#     vasp.indir = outdirs[-1]
-#     density += steps
-
-#     # launch calculations 
-#     outdirs.append("%s/density:%i" % (outdir, density))
-#     output = vasp( structure, outdirs[-1], comm = comm, repat = repat,\
-#                    kpoints=Density(offset, density), **kwargs )
-#     # yields output for whatnot.
-#     yield output
-
-#     # makes sure we don't accidentally converge to early.
-#     oldenergy = output.total_energy
-
-#   # cleanup -- deletes unwanted files from previous output directory
-#   for dir in outdirs:
-#     for file in other_repat:
-#       filename = join(dir, file)
-#       if exists(filename): remove(filename)
-#   
-# def main():
-#   import os.path
-#   import shutil
-#   from boost.mpi import world 
-#   from numpy import array as np_array
-#   from lada import crystal
-#   from lada.vasp import Vasp, Specie
-
-#   structure = crystal.Structure()
-#   structure.cell = np_array( [[1.0,0,0],[0,1,0],[0,0,1]] )
-#   structure.atoms.append( crystal.StrAtom(np_array([0.0,0,0]), "Rb") )
-#   structure.atoms.append( crystal.StrAtom(np_array([0.5,0.5,0.5]), "K") )
-#   structure.name = "KRb_nosym"
-#   structure.scale = 6
-
-#   K = Specie( "K", "~/AtomicPotentials/pseudos/K_s" )
-#   Rb = Specie( "Rb", "~/AtomicPotentials/pseudos/Rb_s" )
-
-#   vasp = Vasp(species=(K, Rb))
-#   if os.path.exists(vasp.indir):  shutil.rmtree( vasp.indir )
-
-#   for output in relaxation( structure, vasp, outdir = "test", comm = world ):
-#     structure = output.structure
-#     print structure.name, structure.energy, "\n", structure
-
-# if __name__ == "__main__":
-#   main()
-
-#     
-
-#     
-#  
+    # Now have vasp do a fake run to create anything it does create.
+    return vasp(structure, outdir, comm, norun=True, **kwargs)
