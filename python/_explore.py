48709946604ee135022d4f4064c6e4e31ac0a77c
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2010-07-23 10:29:36 -0600

    creating ipython package

---
+++ python/_explore.py
@@ -0,0 +1,182 @@
+""" IPython explore function and completer. """
+
+def explore_completer(self, event):
+  """ Completer for explore. """ 
+  from os import getcwd, chdir
+  from os.path import isdir, join
+  from ..jobs import JobDict
+  from .ipy_lada import _get_current_job_params
+
+  ip = self.api
+  current, path = _get_current_job_params(self, 0)
+  print "wtf"
+
+  args = event.symbol.split()
+  if len(args) == 0: 
+    result = [ u + "/" for u in ip.magic("mglob dir:*") ]
+    result.extend([ u for u in ip.magic("mglob \"cont:JobDict pickle\" *") ])
+    result.extend(["errors", "results", "file", "JobDict"])
+  elif len(args) == 1 and args[0].find('/') != -1:
+    orig_dir = getcwd()
+    new_dir = args[0][:-args[0][::-1].find('/')-1]
+    chdir (new_dir)
+    result = [ join(new_dir, u) + "/" for u in ip.magic("mglob dir:*") ]
+    result.extend([ join(new_dir, u) for u in ip.magic("mglob \"cont:JobDict pickle\" *") ])
+    chdir(orig_dir)
+  elif len(args) == 1: 
+    result = [ u + "/" for u in ip.magic("mglob dir:%s*" % (args[0])) ]
+    result.extend([ u for u in ip.magic("mglob \"cont:JobDict pickle\" %s*" % (args[0])) ])
+    result.extend(["errors", "results", "file", "JobDict"])
+    pyargs = args[0].split('.')  
+    dictionary, name = ip.user_ns, None
+    for u in pyargs[:-1]:
+      if u not in dictionary: return results
+      if hasattr(dictionary[u], "__dict__"):
+        dictionary = dictionary[u].__dict__
+        if name == None: name = u
+        else: name += "." + u
+    if name == None: 
+      result.extend([u for u in dictionary if u[0] != '_'])
+    else: 
+      result.extend([name + "." + u for u in dictionary if u[0] != '_'])
+    
+
+  return result
+
+
+def explore(self, arg):
+  """ Starts exploration of a pickled job dictionary. 
+  
+      Usage: 
+      The most standard form is to simply load a job dictionary. All other
+      job-dictionary magic functions will then use it.
+      
+      >>> explore path/to/jobdictionary_pickle
+
+      If you have created a jobdictionary directly (rather than save it to
+      disk), you can also load it as
+
+      >>> explore jobdict_variable 
+
+      In case of conflict between a pathname and a variable name, you can use
+      the more explicit version.
+
+      >>> explore file jobdict
+      >>> explore JobDict jobdict
+
+      You can load a dictionary and filter out successfull or unsuccessfull runs. 
+      To explore errors only, use:
+     
+      >>> explore errors in path/to/job_pickle
+
+      To explore only successful results, use:
+
+      >>> explore results in path/to/job_pickle
+  """
+  from os.path import exists, split as splitpath, abspath, isfile
+  from cPickle import load
+  from .opt import open_exclusive
+  from ..ipy_lada import _get_current_job_params
+  ip = self.api
+  current, path = _get_current_job_params(self, 0)
+  ip.user_ns.pop("_lada_error", None)
+  
+  args = [u for u in arg.split() if u not in ["in", "with"]]
+  if len(args) == 0:
+    if current == None:
+      print "No current jobs."
+    elif path == None:
+      print "Current position in job dictionary:", current.name
+    else:
+      print "Current position in job dictionary:", current.name
+      print "Path to job dictionary: ", path
+    return
+
+  if isinstance(arg, jobs.JobDict):
+    ip.user_ns["current_jobdict"] = arg
+    ip.user_ns["current_jobdict_path"] = None
+    return
+
+  def get_dict(filename, is_a_file = None):
+    if is_a_file == None: 
+      is_a_file = exists(filename)
+      var, dvar = None, ip.user_ns
+      for name in  filename.split('.'):
+        try: var, dvar = dvar[name], dvar[name].__dict__ 
+        except: var = None; break
+      is_a_variable = var != None
+      if is_a_variable and is_a_file:
+        raise RuntimeError(\
+                "Found both a file and a JobDict variable named %s.\n"\
+                "Please use \"explore file %s\" or \"explore JobDict %s\"." \
+                % (filename, filename, filename) )
+      if not (is_a_file or is_a_variable):
+        raise RuntimeError(\
+            "Could not find either JobDict variable or a file with name %s." % (filename) )
+    else: is_a_variable = not is_a_file
+
+    if is_a_file:
+      if not exists(filename): # checks the file exists.
+        raise RuntimeError("Could not find file %s." % (filename))
+      if not isfile(filename):
+        raise RuntimeError("%s is not a file." % (filename))
+      with open_exclusive(filename, "r") as file: result = load(file)
+      return result, abspath(filename)
+
+    if is_a_variable: # checks the variable exists.
+      var, dvar = None, ip.user_ns
+      for name in  filename.split('.'):
+        try: var, dvar = dvar[name], dvar[name].__dict__ 
+        except:
+          raise RuntimeError("Could not find variable name %s." % (filename))
+      if not isinstance(var, jobs.JobDict): # checks the file exists.
+        raise RuntimeError("Variable %s is not a JobDict object." % (filename))
+      return var, None
+
+  def _impl():
+    """ Implementation of explore. """
+    from os.path import join, exists
+    from copy import deepcopy
+    # filters some results, depending on success or failure.
+    if args[0] == "errors" or args[0] == "results":
+      # other arguments should be dictionary.
+      string = ""
+      for i in args[1:]: string += " " + i
+      explore(self, string)
+      current, path = _get_current_job_params(self, 0)
+      if current == None:
+        raise RuntimeError("Error encountered while trying to explore dictionary." )
+      if path == None:
+        raise RuntimeError("No path set for current job dictionary.\n"\
+                           "Please set current_dictionary_path to correct value.\n")
+      ip.user_ns["current_jobdict"] = deepcopy(ip.user_ns["current_jobdict"])
+      rootdir = splitpath(path)[0]
+      # now marks (un)successful runs.
+      which = (lambda x: not x) if args[0] == "results" else (lambda x: x)
+      for job, d in ip.user_ns["current_jobdict"].walk_through():
+        if job.is_tagged: continue
+        directory = join(rootdir, d)
+        if which(job.functional.Extract(directory).success): job.tag()
+        else: job.untag()
+    elif args[0] == "jobs": 
+      string = ""
+      for i in args[1:]: string += " " + i
+      explore(self, string)
+      current, path = _get_current_job_params(self, 0)
+    elif len(args) == 1:                          current, path = get_dict(args[0])
+    elif len(args) == 2 and args[0] == "file":    current, path = get_dict(args[0], True)
+    elif len(args) == 2 and args[0] == "JobDict": current, path = get_dict(args[0], False)
+    else: raise RuntimeError("Calling explore with arguments %s is invalid." % (arg))
+    if current != None: 
+      ip.user_ns["current_jobdict"] = current
+      ip.user_ns["current_jobdict_path"] = path
+
+  try: _impl()
+  except RuntimeError as e:
+    ip.user_ns["_lada_error"] = e
+    print e
+  else:
+    # remove any prior iterator stuff.
+    ip.user_ns.pop("_lada_subjob_iterator", None)
+    ip.user_ns.pop("_lada_subjob_iterated", None)
+
---
+++ python/_goto.py
@@ -0,0 +1,125 @@
+""" IPython goto and iterate magic functions. """
+from contextlib  import contextmanager
+
+def goto(self, arg):
+  """ Moves current dictionary position and working directory (if appropriate). """
+  from os import chdir
+  from os.path import exists, join, split as splitpath
+  from ..ipy_lada import _get_current_job_params
+  ip = self.api
+  current, path = _get_current_job_params(self, 1)
+  ip.user_ns.pop("_lada_error", None)
+    
+  if current == None: 
+    print "No current jobs."
+    return
+  if len(arg.split()) == 0:
+    if path == None:
+      print "Current position in job dictionary:", current.name
+    else:
+      print "Current position in job dictionary:", current.name
+      print "Filename of jobdictionary: ", path
+    return
+  args = arg.split()
+  if len(args) > 1:
+    ip.user_ns["_lada_error"] = "Invalid argument to goto %s." % (arg)
+    print ip.user_ns["_lada_error"]
+    return
+
+  # if no argument, then print current job data.
+  if len(args) == 0: 
+    explore(self, "")
+    return
+
+  # cases to send to iterate.
+  if args[0] == "next":       return iterate(self, "")
+  elif args[0] == "previous": return iterate(self, "previous")
+  elif args[0] == "reset":    return iterate(self, "reset")
+
+  # case for which precise location is given.
+  try: result = current[args[0]] 
+  except KeyError as e: 
+    print e
+    ip.user_ns["_lada_error"] = e
+    return 
+
+  if not (hasattr(result, "parent") and hasattr(result, "children")\
+     and hasattr(result, "jobparams")):
+    ip.user_ns["_lada_error"] = "%s is a job parameter, not a directory."
+    print ip.user_ns["_lada_error"] 
+    return
+  ip.user_ns["current_jobdict"] = result
+  current = ip.user_ns["current_jobdict"]
+  if path == None: return
+  dir = join(splitpath(path)[0], current.name[1:]) 
+  if exists(dir): chdir(dir)
+  return
+
+
+def iterate(self, event):
+  """ Goes to next (untagged) job. """
+  from ..ipy_lada import _get_current_job_params
+  ip = self.api
+  current, path = _get_current_job_params(self, 1)
+  if current == None: return
+
+  args = event.split()
+  if len(args) > 1: 
+    print "Invalid argument %s." % (event)
+  elif len(args) == 0:
+    if "_lada_subjob_iterator" in ip.user_ns: 
+      iterator = ip.user_ns["_lada_subjob_iterator"]
+    else:
+      iterator = current.root.walk_through()
+    while True:
+      try: job, d = iterator.next()
+      except StopIteration: 
+        print "Reached end of job list."
+        return 
+      if job.is_tagged: continue
+      break
+    ip.user_ns["_lada_subjob_iterator"] = iterator
+    if "_lada_subjob_iterated" not in ip.user_ns: ip.user_ns["_lada_subjob_iterated"] = []
+    ip.user_ns["_lada_subjob_iterated"].append(current.name)
+    goto(self, job.name)
+    print "In job ", ip.user_ns["current_jobdict"].name
+  elif args[0] == "reset" or args[0] == "restart":
+    # remove any prior iterator stuff.
+    ip.user_ns.pop("_lada_subjob_iterator", None)
+    ip.user_ns.pop("_lada_subjob_iterated", None)
+    print "In job ", ip.user_ns["current_jobdict"].name
+  elif args[0] == "back" or args[0] == "previous":
+    if "_lada_subjob_iterated" not in ip.user_ns: 
+      print "No previous job to go to. "
+    else:
+      goto(self, ip.user_ns["_lada_subjob_iterated"].pop(-1))
+      if len(ip.user_ns["_lada_subjob_iterated"]) == 0:
+        del ip.user_ns["_lada_subjob_iterated"]
+      print "In job ", ip.user_ns["current_jobdict"].name
+
+
+def goto_completer(self, event):
+  import IPython
+  from ..ipy_lada import _get_current_job_params
+  ip = self.api
+  current, path = _get_current_job_params(self, 0)
+  if current == None: raise IPython.ipapi.TryNext
+
+  elif '/' in event.symbol:
+    # finds last '/' in string.
+    subkey = event.symbol[:-event.symbol[::-1].find('/')-1]
+    while subkey[-1] == '/': subkey = subkey[:-1]
+    try: subdict = current[subkey] 
+    except KeyError: raise IPython.ipapi.TryNext
+    if hasattr(subdict, "children"): 
+      if hasattr(subdict.children, "keys"):
+        return [subkey + "/" + a + "/" for a in subdict.children.keys()]
+    raise IPython.ipapi.TryNext
+  else:
+    result = [a + "/" for a in current.children.keys()]
+    result.extend(["/", "next", "reset"])
+    if current.parent != None: result.append("../")
+    if "_lada_subjob_iterated" in ip.user_ns:
+      if len(ip.user_ns["_lada_subjob_iterated"]): result.append("previous")
+    return result
+
