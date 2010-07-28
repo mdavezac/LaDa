79a43d7ee8cad5661e1c9920b84b4768497a41d7
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2010-07-28 10:07:59 -0600

    Checked that emass test is correct (serial version)

--- test/escan/emass.py
+++ test/escan/emass.py
@@ -1,3 +1,4 @@
+from math import fabs as abs
 from numpy import array
 from boost.mpi import world
 from lada.crystal import fill_structure
@@ -14,11 +15,12 @@ structure = fill_structure(input.vff.lattice.cell, input.vff.lattice)
 input.escan.nbstates = nb_valence_states(structure) + 4
 input.escan.tolerance = 1e-12
 
-print nb_valence_states(structure) 
-
 result = reciprocal( input.escan, structure, X, outdir="work/emass/100", \
                      comm = world, order = 2, _dont_deform_kpoint=True )
-print result
+assert abs(1e0 / result[0][2,8] - 0.4381) < 0.01 # Gamma conduction emass 
+assert abs(-1e0 / result[0][2,7] - 0.2769) < 0.01 # Gamma conduction heavy hmass (100 direction)
+assert abs(-1e0 / result[0][2,5] - 0.2059) < 0.01 # Gamma conduction light hmass (100 direction)
 result = reciprocal( input.escan, structure, array([1e0, 1, 1]), outdir="work/emass/111", \
                      comm = world, order = 2, _dont_deform_kpoint=True )
-print result
+assert abs(-1e0 / result[0][2,7] - 0.6885) < 0.01 # Gamma conduction heavy hmass (111 direction)
+assert abs(-1e0 / result[0][2,5] - 0.1460) < 0.01 # Gamma conduction light hmass (111 direction)
