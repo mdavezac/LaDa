72bddb01ed92d796f6db2139ba2f33d2613f13f7
Mayeul d'Avezac <mdavezac@gmail.com>
2010-08-15 17:47:40 -0600

    incorrect use of __dir__ rather than __all__.

--- pcm/python/__init__.py
+++ pcm/python/__init__.py
@@ -3,5 +3,5 @@ __docformat__ = "restructuredtext en"
 
 from _pcm import Charges, LJBond, LJBonds, Clj, bond_name, minimize
 
-__dir__ = [ "Charges", "LJBond", "LJBonds", "Clj", "bond_name", "minimize" ]
+__all__ = [ "Charges", "LJBond", "LJBonds", "Clj", "bond_name", "minimize" ]
 
