891a7c552685eb99941531e588a0c3d6d7462167
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2010-11-08 14:56:28 -0700

    Revert "switching b5 around"
    This reverts commit e5e6b3b2c406b2804a7bb5dec0c4ea1dd149c3ad.

--- crystal/python/A2BX4.py
+++ crystal/python/A2BX4.py
@@ -553,18 +553,35 @@ def b4():
   lattice.find_space_group()
   return lattice
 
-def b5D(u=0.25):
+def b5D():
   """ Returns b5D lattice."""
   from . import Lattice
-  lattice = lattice.b5(u)
-  lattice.sites[0].type = ['B']
-  lattice.sites[4].type = ['A']
+  
+  lattice = Lattice()
+  lattice.scale = 1.000000e+00
+  lattice.set_cell = (0.000000e+00, 5.000000e-01, 5.000000e-01),\
+                     (5.000000e-01, 0.000000e+00, 5.000000e-01),\
+                     (5.000000e-01, 5.000000e-01, 0.000000e+00)
+  lattice.name = "b5D"
+  lattice.add_site = (5.000000e-01, 5.000000e-01, 5.000000e-01), "A", 
+  lattice.add_site = (5.000000e-01, 2.500000e-01, 2.500000e-01), "A", 
+  lattice.add_site = (2.500000e-01, 5.000000e-01, 2.500000e-01), "A", 
+  lattice.add_site = (8.750000e-01, 8.750000e-01, 8.750000e-01), "A", 
+  lattice.add_site = (2.500000e-01, 2.500000e-01, 5.000000e-01), "B", 
+  lattice.add_site = (1.250000e-01, 1.250000e-01, 1.250000e-01), "B", 
+  lattice.add_site = (2.500000e-01, 2.500000e-01, 2.500000e-01), "X", 
+  lattice.add_site = (2.500000e-01, 5.000000e-01, 5.000000e-01), "X", 
+  lattice.add_site = (5.000000e-01, 2.500000e-01, 5.000000e-01), "X", 
+  lattice.add_site = (5.000000e-01, 5.000000e-01, 2.500000e-01), "X", 
+  lattice.add_site = (7.500000e-01, 7.500000e-01, 7.500000e-01), "X", 
+  lattice.add_site = (7.500000e-01, 5.000000e-01, 5.000000e-01), "X", 
+  lattice.add_site = (5.000000e-01, 7.500000e-01, 5.000000e-01), "X", 
+  lattice.add_site = (5.000000e-01, 5.000000e-01, 7.500000e-01), "X", 
   lattice.find_space_group()
   return lattice
 
 def b5(u=0.25):
   """ Returns b5 lattice."""
-  from numpy import dot
   from . import Lattice
 
   x = u
@@ -592,7 +609,6 @@ def b5(u=0.25):
   lattice.add_site = (    -x,    -y,    -y), "X",
   lattice.add_site = (    -y,    -x,    -y), "X",
   lattice.add_site = (    -y,    -y,    -x), "X",
-  for site in lattice.sites: site.pos = dot(lattice.cell, site.pos)
   lattice.find_space_group()
 
   return lattice
@@ -1071,10 +1087,14 @@ def b5I(u=0.25):
   from . import Lattice
   
   lattice = b5(u)
-  lattice.sites[0].type = ['B']
-  lattice.sites[1].type = ['B']
-  lattice.sites[4].type = ['A']
-  lattice.sites[5].type = ['A']
+  nb_B = 2
+  for site in lattice.sites:
+    if "A" in site.type and nb_B > 0: 
+      site.type = ["B"]
+      nb_B -= 1
+    elif "B" in site.type: site.type = ["A"]
+  lattice.name = "b5I"
+  lattice.find_space_group()
   return lattice
 
 
