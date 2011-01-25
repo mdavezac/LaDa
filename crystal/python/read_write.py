c93bbdb1d850cd19f2471110eaa1dace9c912393
Vladan Stevanovic <vstevano@pupin.site>
2011-01-25 15:43:29 -0700

    Added the icsd_cif to crystal package

--- crystal/python/read_write.py
+++ crystal/python/read_write.py
@@ -1,6 +1,6 @@
 """ Congregates all read/write python routines. """
 __docformat__ = "restructuredtext en"
-__all__ = ['read_poscar', 'write_poscar', 'write_oldvff', 'read_oldvff']
+__all__ = ['read_poscar', 'write_poscar', 'write_oldvff', 'read_oldvff', 'icsd_cif']
 from lada.opt.decorators import broadcast_result
 
 @broadcast_result(key=True)
@@ -233,3 +233,190 @@ def read_oldvff(path):
   else: result.scale = 1 
 
   return result
+
+
+@broadcast_result(key=True)
+def icsd_cif(filename): 
+  """ Reads lattice from the ICSD *cif files.
+
+      It will not work in the case of other *cif. 
+      It's likely to produce wrong output if the site occupations are fractional.
+      If the occupation is > 0.5 it will treat it as 1 and 
+      in the case occupation < 0.5 it will treat it as 0 and 
+      it will accept all occupation = 0.5 as 1 and create a mess!
+  """
+  import re
+  from os.path import basename
+  from numpy.linalg import norm
+  from numpy import array, transpose
+  from numpy import pi, sin, cos, sqrt, dot
+
+  lines = open(filename,'r').readlines()
+
+  sym_big = 0
+  sym_end = 0
+  pos_big = 0 
+  pos_end = 0
+
+  for l in lines:
+      x = l.split()
+      if len(x)>0:
+          # CELL
+          if x[0] == '_cell_length_a':
+              if '(' in x[-1]:
+                  index = x[-1].index('(')
+              else:
+                  index = len(x[-1])
+              a = float(x[-1][:index])
+              
+          if x[0] == '_cell_length_b':
+              if '(' in x[-1]:
+                  index = x[-1].index('(')
+              else:
+                  index = len(x[-1])
+              b = float(x[-1][:index])
+
+          if x[0] == '_cell_length_c':
+              if '(' in x[-1]:
+                  index = x[-1].index('(')
+              else:
+                  index = len(x[-1])
+              c = float(x[-1][:index])
+
+          if x[0] == '_cell_angle_alpha':
+              if '(' in x[-1]:
+                  index = x[-1].index('(')
+              else:
+                  index = len(x[-1])
+              alpha = float(x[-1][:index])
+              
+          if x[0] == '_cell_angle_beta':
+              if '(' in x[-1]:
+                  index = x[-1].index('(')
+              else:
+                  index = len(x[-1])
+              beta = float(x[-1][:index])
+
+          if x[0] == '_cell_angle_gamma':
+              if '(' in x[-1]:
+                  index = x[-1].index('(')
+              else:
+                  index = len(x[-1])
+              gamma = float(x[-1][:index])
+
+      # SYMMETRY OPERATIONS
+
+      if len(x)>0 and x[0] == '_symmetry_equiv_pos_as_xyz':
+          sym_big = lines.index(l)
+
+      if len(x)>0 and x[0] == '_atom_type_symbol':
+          sym_end = lines.index(l)
+
+      # WYCKOFF POSITIONS
+      
+      if len(x)>0 and x[0] == '_atom_site_attached_hydrogens':
+          pos_big = lines.index(l)
+
+      if len(x)>0 and x[0] == '_atom_site_B_iso_or_equiv':
+          pos_big = lines.index(l)
+
+      if len(x)>0 and x[0] == '_atom_site_U_iso_or_equiv':
+          pos_big = lines.index(l)
+
+      if len(x)>0 and x[0] == '_atom_site_0_iso_or_equiv':
+          pos_big = lines.index(l)
+
+      if pos_end == 0 and l == '\n' and lines.index(l) > pos_big:
+          pos_end = lines.index(l)
+
+  symm_ops = ['(' + x.split()[1][1:] + x.split()[2] + x.split()[3][:-1] + ')' for x in lines[sym_big+1:sym_end-1]]
+
+  symm_ops = [re.sub(r'(\d+)', r'\1.', x) for x in symm_ops]
+  
+  wyckoff = [[x.split()[0],[x.split()[4],x.split()[5],x.split()[6]],x.split()[7]] for x in lines[pos_big+1:pos_end]]
+  
+  wyckoff = [w for w in wyckoff if int(float(w[-1][:4])+0.5) != 0]
+
+############## Setting up a good wyckoff list
+
+  for w in wyckoff:
+      pom = 0
+      for i in range(len(w[0])):
+          try:
+              int(w[0][i])
+              if pom ==0: pom=i
+          except:
+              pass
+
+      w[0] = w[0][:pom]
+
+      for i in range(3):
+          if '(' in w[1][i]: 
+              index = w[1][i].index('(')
+          else:
+              index = len(w[1][i])
+          w[1][i] = float(w[1][i][:index])
+          
+      del w[-1]
+##########################################
+  symbols = list(set([w[0] for w in wyckoff]))
+  positions = [[] for i in range(len(symbols))]
+
+  for w in wyckoff:
+      symbol = w[0]
+      x,y,z = w[1][0],w[1][1],w[1][2]
+      for i in range(len(symm_ops)):
+          pom = list(eval(symm_ops[i]))
+          for j in range(len(pom)):
+              if pom[j] <  0.: pom[j] = pom[j]+1.
+              if pom[j] >= 0.999: pom[j] = pom[j]-1.
+
+          if not any(norm(array(u)-array(pom)) < 0.01 for u in positions[symbols.index(symbol)]):
+              positions[symbols.index(symbol)].append(pom)
+
+################ CELL ####################
+
+  a1 = a*array([1.,0.,0.])
+  a2 = b*array([cos(gamma*pi/180.),sin(gamma*pi/180.),0.])
+  c1 = c*cos(beta*pi/180.)
+  c2 = c/sin(gamma*pi/180.)*(-cos(beta*pi/180.)*cos(gamma*pi/180.) + cos(alpha*pi/180.))
+  a3 = array([c1, c2, sqrt(c**2-(c1**2+c2**2))])
+  cell = array([a1,a2,a3])
+
+##########################################
+
+  from lada.crystal import Lattice
+  lattice = Lattice()
+  lattice.scale = 1.0
+  lattice.name = basename(filename)
+  lattice.set_cell = transpose(cell)
+
+  for i in range(len(symbols)):
+      for j in range(len(positions[i])):
+          lattice.add_site = dot(transpose(cell),positions[i][j]), symbols[i]
+
+  lattice.make_primitive()
+
+  return lattice
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
