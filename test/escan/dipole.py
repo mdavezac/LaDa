18ed5056a25c575b982f4f8db458cf3b06fda91a
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2011-03-01 09:20:17 -0700

    Corrected dipole check.
      We have to sum dipole moments across degenerate states. Otherwise, it
    is not assured that the results is reproducible since degenerate states
    found by escan in one run could different from the states found in
    another run, as long as they span the same space.

--- test/escan/dipole.py
+++ test/escan/dipole.py
@@ -5,7 +5,7 @@ from numpy.linalg import det
 from quantities import angstrom, eV
 from boost.mpi import world
 from lada.escan import read_input, bandgap
-from lada.crystal import fill_structure, sort_layers, nb_valence_states
+from lada.crystal import fill_structure, sort_layers, nb_valence_states, Structure
 from lada.crystal.binary import zinc_blende
 from lada.physics import a0, reduced_reciprocal_au
 from numpy import pi
@@ -13,29 +13,20 @@ from numpy import pi
 input = read_input("input.py")
 cell = [[2.5,0,0],[0.5,0.5,0.5],[0,-0.5,0.5]]
 subs = {'A':'Si', 'B':'Ge'}
+
 structure = zinc_blende().to_structure(cell, subs)
-structure.scale = float(5.34594034 / 0.5 * a0.rescale(angstrom))
+structure.scale = float(5.33847592 / 0.5 * a0.rescale(angstrom))
 structure = sort_layers(structure, array([2.5,0,0]))
 for i, atom in enumerate(structure.atoms):
   atom.type = 'Si' if i % 10 < 6 else 'Ge'
 
-input.escan.fft_mesh = 50, 14, 45
-input.escan.vff.direction = None
-
 result = bandgap( input.escan, structure, comm=world, eref=None, 
                   outdir="result/dipoles", references = (-0.2, 0.2),
-                  nbstates=6, direction = array([1., 0,0]) )
-volume = det(result.structure.cell*result.structure.scale/a0.rescale(angstrom))
-cbms = result.extract_cbm.gwfns 
-# cbms = sorted(cbms, key=attrgetter("eigenvalue"))
-vbms = result.extract_vbm.gwfns 
-# vbms = sorted(vbms, key=attrgetter("eigenvalue"))
-units = pi ** 2 # / 4. #float(a0.rescale(angstrom) * a0.rescale(angstrom))
-dips = []
-for e0, e1, u in result.dipole():
-  dips.append((e0, e1, (u * u.conjugate()).real))
-check = [(array(0.168325) * eV, array(-0.49477701000000002) * eV, array([  1.43562357e-04,   1.04201091e-05,   1.95678721e-05]) * 1/a0**2), (array(0.168325) * eV, array(-0.49477701000000002) * eV, array([  4.20854765e-04,   1.67207771e-05,   7.57293883e-06]) * 1/a0**2), (array(0.168325) * eV, array(-0.49477701000000002) * eV, array([  4.20854765e-04,   1.67207771e-05,   7.57293883e-06]) * 1/a0**2), (array(0.168325) * eV, array(-0.49477701000000002) * eV, array([  1.43562357e-04,   1.04201091e-05,   1.95678721e-05]) * 1/a0**2)]
-for (e0, e1, a), (E0, E1, A) in zip(dips, check):
-  assert abs(e0-E0) < 1e-6
-  assert abs(e1-E1) < 1e-6
-  assert all(abs(a-A) < 1e-6)
+                  nbstates=4, direction = array([1., 0,0]),
+                  fft_mesh = (50,14,14) )
+
+tot = array([0e0] * 3)  / a0 / a0
+for e0, e1, u in result.dipole(degeneracy = 5e1 * input.escan.tolerance, attenuate=False):
+  tot += (u * u.conjugate()).real
+check = array([1.13781377e-03,   5.90701883e-05,   5.90701874e-05]) * 1/a0**2
+assert all( abs(check - tot) < 5e1*input.escan.tolerance )
