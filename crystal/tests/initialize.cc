91a6c080d145d84a588bf903d9cbf3a5683bc7d6
Mayeul d'Avezac <mdavezac@gmail.com>
2011-07-29 23:13:34 -0600

    Trying to simplify test script and scripting some more

--- crystal/tests/initialize.cc
+++ crystal/tests/initialize.cc
@@ -8,29 +8,29 @@
 #include "../is_container.h"
 #include <opt/debug.h>
 
-#if LADA_STRUCTURE == 0
-#  define T_STR StructureData
-#else
-#  define T_STR TemplateStructure
-#endif
-#if LADA_TYPE == 0
-#  define T_TYPE std::string
-#else
-#  define T_TYPE std::vector<std::strin>
-#endif
-
+using namespace std;
 int main()
 {
   using namespace LaDa::crystal;
-  T_STR<T_TYPE> structure;
+# if LADA_TEST_INCTYPE == 0
+    LADA_TEST_STRUCTURE<std::string> structure;
+# elif LADA_TEST_INCTYPE == 1
+    LADA_TEST_STRUCTURE< std::vector<std::string> > structure;
+# elif LADA_TEST_INCTYPE == 2
+    LADA_TEST_STRUCTURE<int> structure;
+# endif
   structure.set_cell(-0.5,0.5,0.5)
-               (0.5,-0.5,0.5)
-               (0.5,0.5,-0.5);
-  structure.add_atom(0,0,0, "Au");
-# if LADA_TYPE == 0
+                    (0.5,-0.5,0.5)
+                    (0.5,0.5,-0.5);
+# if LADA_TEST_INCTYPE == 0
+    structure.add_atom(0,0,0, "Au");
     structure.add_atom(0.25,0.25,0.25, "Pd");
-# else
+# elif LADA_TEST_INCTYPE == 1
+    structure.add_atom(0,0,0, "Au");
     structure.add_atom(0.25,0.25,0.25, "Au", "Pd");
+# elif LADA_TEST_INCTYPE == 2
+    structure.add_atom(0,0,0, 1);
+    structure.add_atom(0.25,0.25,0.25, 15);
 # endif
   for(size_t i(0); i < 3; ++i)
   {
@@ -40,15 +40,18 @@ int main()
     LADA_DOASSERT(structure[0].pos[i] == 0, "structure atom data incorrect.");
     LADA_DOASSERT(structure[1].pos[i] == 0.25, "structure atom data incorrect.");
   }
-# if LADA_TYPE == 0
+# if LADA_TEST_INCTYPE == 0
     LADA_DOASSERT(structure[0].type == "Au", "structure atom data incorrect.");
     LADA_DOASSERT(structure[1].type == "Pd", "structure atom data incorrect.");
-# else
+# elif LADA_TEST_INCTYPE == 1
     LADA_DOASSERT(structure[0].type.size() == 1, "structure atom data incorrect.");
     LADA_DOASSERT(structure[0].type[0] == "Au", "structure atom data incorrect.");
     LADA_DOASSERT(structure[1].type.size() == 2, "structure atom data incorrect.");
     LADA_DOASSERT(structure[1].type[0] == "Au", "structure atom data incorrect.");
     LADA_DOASSERT(structure[1].type[1] == "Pd", "structure atom data incorrect.");
+# elif LADA_TEST_INCTYPE == 2
+    LADA_DOASSERT(structure[0].type == 1, "structure atom data incorrect.");
+    LADA_DOASSERT(structure[1].type == 15, "structure atom data incorrect.");
 # endif
   return 0;
 }
