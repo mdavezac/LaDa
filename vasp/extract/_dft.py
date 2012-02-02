32f5bd27e40dc46933ae7a36aa1a91f4f08817e2
Mayeul d'Avezac <mayeul.davezac@nrel.gov>
2012-02-02 11:13:11 -0700

    Bug in vasp.extrat.fermi0K

--- vasp/extract/_dft.py
+++ vasp/extract/_dft.py
@@ -73,7 +73,7 @@ class Extract(object):
     array = [(e, m) for u, m in zip(eigs, self.multiplicity) for e in u.flat]
     array = sorted(array, key=itemgetter(0))
     occ = 0e0
-    factor = 2e0/float(sum(self.multiplicity))
+    factor = (2.0 if self.ispin == 1 else 1.0)/float(sum(self.multiplicity))
     for i, (e, w) in enumerate(array):
       occ += w*factor
       if occ >= self.valence - 1e-8: return e * self.eigenvalues.units
