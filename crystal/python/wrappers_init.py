492933d53dd648c5de4f09bb82b109339f2f2ad6
Mayeul d'Avezac <mdavezac@gmail.com>
2012-05-01 18:54:50 -0600

    process.pool seems to work
       * process.pool allows for running executable folders in parallel when those
         folders require different number of processes.
       * removed old files from crystal

--- crystal/python/wrappers_init.py
+++
@@ -1,1 +0,0 @@
-""" Subpackage with C++ wrappers. """
--- process/pool.py
+++ process/pool.py
@@ -6,48 +6,20 @@ class PoolProcess(JobFolderProcess):
       parallel, with up to :py:attr:`~PoolProcess.nbpools` running
       instances. Each instance is allocated an equal number of machines.
   """
-  def __init__(self, jobfolder, outdir, maxtrials=1, processalloc=None, **kwargs):
+  def __init__(self, jobfolder, outdir, processalloc, maxtrials=1, **kwargs):
     """ Initializes a process. """
     super(PoolProcess, self).__init__(jobfolder, outdir, maxtrials, **kwargs)
     del self.nbpools # not needed here.
 
     self.processalloc = processalloc
     """ How many jobs to launch simultaneously. """
+    self._alloc = {}
+    """ Maps job vs rquested process allocation. """
+    for name in self._torun:
+      self._alloc[name] = self.processalloc(self.jobfolder[name])
+    assert len(set(self._alloc.keys())) == len(self._alloc)
 
-  @property
-  def nbjobsleft(self): 
-    """ Number of jobs left. """
-    return len(self._torun)
 
-  def poll(self): 
-    """ Polls current job. """
-    from . import Fail
-
-    if super(PoolProcess, self).poll(): return True
-
-    # weed out successfull and failed jobs.
-    finished = []
-    for i, (name, process) in enumerate(list(self.process)):
-      try:
-        if process.poll() == True: 
-          self._finished.add(name)
-          finished.append(i)
-      except Fail as fail:
-        self.errors[name] = fail
-        finished.append(i)
-    for i in sorted(finished)[::-1]:
-      name, process = self.process.pop(i)
-      process._cleanup()
-
-    # returns True if nothing left to do.
-    if len(self.process) == 0 and len(self._torun) == 0:
-      if len(self.errors) == 0: return True
-      else: raise Fail()
-
-    # adds new jobs. 
-    self._next()
-    return False
- 
   def _next(self):
     """ Adds more processes.
     
@@ -61,18 +33,20 @@ class PoolProcess(JobFolderProcess):
 
     # nothing else to do.
     if len(self._torun) == 0: return
-    # cannot add more processes.
-    if len(self.process) >= self.nbpools: return
     # no more machines to allocate...
     if self._comm['n'] == 0: return
+    # cannot add more processes.
+    if isinstance(self.processalloc, int):
+      return super(PoolProcess, self)._next()
 
     # split processes into local comms. Make sure we don't oversuscribe.
     jobs = self._getjobs()
+    assert sum(self._alloc[u] for u in jobs) <= self._comm['n']
     try: 
       # Loop until all requisite number of processes is created, 
       # or until run out of jobs, or until run out of comms. 
       for name in jobs:
-        self._torun.pop(self._torun.index(name))
+        self._torun = self._torun - set([name])
         nprocs = self._alloc.pop(name)
         # checks folder is still valid.
         if name not in self.jobfolder:
@@ -91,65 +65,23 @@ class PoolProcess(JobFolderProcess):
           process = CallProcess(self.functional, join(self.outdir, name), **params)
         # appends process and starts it.
         self.process.append((name, process))
-        process.start(self._comm.acquire(nprocs))
+        process.start(self._comm.lend(nprocs))
     except:
       self.terminate()
       raise
 
-  def kill(self):
-    """ Kills all currently running processes. """
-    for name, process in self.process: process.kill()
-    self._cleanup()
-    self.process = []
-    
-  def terminate(self):
-    """ Kills all currently running processes. """
-    for name, process in self.process: process.terminate()
-    self._cleanup()
-    self.process = []
-
-  @property 
-  def done(self):
-    """ True if job already finished. """
-    return self.started and len(self.process) == 0
-  @property
-  def nbrunning_processes(self):
-    """ Number of running processes. 
-
-        For simple processes, this will be one or zero.
-        For multitasking processes this may be something more.
-    """
-    return 0 if (not self.started) or len(self.process) == 0 else 1
-
-  def wait(self, sleep=0.5):
-    """ Waits for all job-folders to execute and finish. """
-    from time import sleep as time_sleep
-    if super(PoolProcess, self).wait(): return True
-    while not self.poll(): 
-      if sleep > 0: time_sleep(sleep)
-    return False
-
-  def _cleanup(self):
-    """ Cleans up after currently running processes. """
-    try: 
-      for name, process in self.process: process._cleanup()
-    finally:
-      self.process = []
-      if hasattr(self, '_comm'): 
-        try: self._comm.cleanup()
-        finally: del self._comm
-
   def _getjobs(self):
     """ List of jobs to run. """
     from operator import itemgetter
-    N = self._comm['N']
+    N = self._comm['n']
 
     # creates list of possible jobs.
     availables = sorted( [(key, u) for key, u in self._alloc.iteritems() if u <= N],
                          key=itemgetter(1) )
+    if len(availables) == 0: return []
 
     # hecks first if any jobs fits exactly the available number of nodes.
-    if availables[-1][1] == N: return [availables[-1][1]]
+    if availables[-1][1] == N: return [availables[-1][0]]
 
     # creates a map of bins: 
     bins = {}
@@ -168,8 +100,8 @@ class PoolProcess(JobFolderProcess):
         if newn == 0:
           yield xvec + [(key, u)], True
           break
-        for key in list(newbins.keys()):
-          if newbins[key] > newn: del newbins[key]
+        for v in list(newbins.keys()):
+          if v > newn: del newbins[v]
         if len(newbins) == 0:
           yield xvec + [(key, u)], False
           continue
@@ -204,13 +136,8 @@ class PoolProcess(JobFolderProcess):
 
     if Process.start(self, comm): return True
 
-    # maps jobs and requested allocation
-    self._alloc = {}
-    for key, job in self.jobfolder.iteritems(): 
-      self._alloc[key] = self.mppalloc(job)
-
     # check max job size.
-    toolarge = [key for key, u in self._alloc.iteritems() if u > comm['u']]
+    toolarge = [key for key, u in self._alloc.iteritems() if u > comm['n']]
     if len(toolarge):
       raise MPISizeError( "The following jobs require too many processors:\n"\
                                 "{0}\n".format(toolarge) )
