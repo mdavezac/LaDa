b9cd0490600dcf452b773ca0a32eb0415b23d818
Mayeul d'Avezac <m.davezac@imperial.ac.uk>
2012-08-10 17:49:13 +0100

    working on method to create clusters

--- ce/__init__.py
+++ ce/__init__.py
@@ -1,4 +1,5 @@
 """ Cluster Expansion types and methods. """
 __docformat__ = "restructuredtext en"
-__all__ = ['Cluster']
+__all__ = ['Cluster', 'cluster_factory']
 from .cluster import Cluster
+from .factory import cluster_factory
