b72b11e5f0b903d678c608f19f6587818b40e5a4
Mayeul d'Avezac <m.davezac@imperial.ac.uk>
2012-11-14 19:37:27 +0000

    Functional gradients seem to work in VFF

--- vff/functional.py
+++ vff/functional.py
@@ -5,14 +5,14 @@ from .extract import Extract as ExtractVFF
 class Functional(Vff): 
   Extract = ExtractVFF
   """ Extraction object for Vff. """
-  def __init__(self, relax=True, method='BFGS', tol=1e-8, maxiter=50): 
+  def __init__(self, relax=True, method='BFGS', tol=1e-8, maxiter=100, copy=None): 
     super(Functional, self).__init__()
 
     self._parameters = {}
     """ Holds vff parameters. """
     self.relax = relax
     """ Whether to relax the structure """
-    self.methods = method
+    self.method = method
     """ Type of method used to relax the structure. 
     
         .. see:: 
@@ -25,6 +25,8 @@ class Functional(Vff):
     """ Convergence criteria. """
     self.maxiter = maxiter
     """ Maximum number of iterations. """
+
+    if copy is not None: self.__dict__.update(copy.__dict__)
 
 
   def _is_static(self, **kwargs):
@@ -40,6 +42,7 @@ class Functional(Vff):
     """ Evaluates energy and forces on a structure. """
     from datetime import datetime
     from os.path import join
+    from scipy.optimize import minimize
     from .extract import Extract as ExtractVFF
 
     if ExtractVFF(outdir).success and not overwrite: return ExtractVFF(outdir)
@@ -61,7 +64,13 @@ class Functional(Vff):
       if self._is_static(**kwargs):
         result = super(Functional, self).__call__(structure)
       else: 
-        result, minimization = self._relax_all(structure)
+        funcs = self._getfuncs_relaxall(structure)
+        minimization = minimize( funcs.energy, jac=funcs.jacobian, x0=funcs.x0, 
+                                 tol=self.tol, method=self.method,
+                                 options={'maxiter': self.maxiter} )
+        result = funcs.structure(minimization.x)
+        # this will reconstruct the tree and might fail. 
+        result = super(Functional, self).__call__(result)
     finally: 
       with open(join(outdir, 'vff.out'), 'a') as file:
         if minimization is not None:
@@ -79,17 +88,18 @@ class Functional(Vff):
     return ExtractVFF(outdir)
 
 
-  def _relax_all(self, structure):
+  def _getfuncs_relaxall(self, structure):
+    """ Functions when relaxing all degrees of freedom. """
+    from collections import namedtuple
     from numpy import dot, array, zeros
     from numpy.linalg import inv, det
-    from scipy.optimize import minimize
     from quantities import angstrom
     from . import build_tree
 
     structure = structure.copy()
-    cell0 = structure.cell.copy()
+    cell0, pos0 = structure.cell.copy(), structure[0].pos
     tree = build_tree(structure)
-    invscale = 1e0 / float(structure.scale.rescale(angstrom))
+    scale = float(structure.scale.rescale(angstrom))
 
     def xtostrain(x0):
       return array([[x0[0] + 1e0, x0[1], x0[2]],
@@ -98,8 +108,14 @@ class Functional(Vff):
       
     def update_structure(x0, strain):
       structure.cell = dot(strain, cell0)
+      structure[0].pos = dot(strain, pos0)
       for i, atom in enumerate(structure[1:]):
         atom.pos = dot(structure.cell, x0[i*3:3+i*3])
+
+    def make_structure(x0):
+      strain = xtostrain(x0)
+      update_structure(x0[6:], strain)
+      return structure
 
     def energy(x0):
       strain = xtostrain(x0)
@@ -114,20 +130,22 @@ class Functional(Vff):
       stress *= -det(structure.scale * structure.cell)
 
       stress = dot(stress, inv(strain))
-      result = stress[0].tolist() + stress[1,1:].tolist() + [stress[2,2]]
-      result += dot(inv(structure.cell)*invscale, forces[1:].T).T.flatten().tolist()
-      return array(result)
+      result = zeros(3+len(structure)*3, dtype='float64')
+      result[0] = stress[0,0]
+      result[1] = 2e0*stress[0,1]
+      result[2] = 2e0*stress[0, 2]
+      result[3] = stress[1,1]
+      result[4] = 2e0*stress[1,2]
+      result[5] = stress[2, 2]
+      result[6:] = dot(structure.cell.T*scale, forces[1:].T).T.flatten()
+      return result
 
     x = zeros(3+len(structure)*3, dtype='float64')
-    x[:6] = 0e0
     frac = inv(structure.cell)
     for i, atom in enumerate(structure[1:]): x[6+3*i:9+3*i] = dot(frac, atom.pos)
 
-    result = minimize( energy, jac=jacobian, x0=x, 
-                       tol=self.tol, options={'maxiter': self.maxiter} )
-    strain = xtostrain(result.x)
-    update_structure(result.x[6:], strain)
-    return super(Functional, self).__call__(structure), result
+    Functions = namedtuple('Functions', ['x0', 'jacobian', 'energy', 'structure'])
+    return Functions(x, jacobian, energy, make_structure)
 
 del stateless
 del assign_attributes
---
+++ vff/tests/gradientfunc.py
@@ -0,0 +1,79 @@
+def functional():
+  from lada.vff.vff import Vff
+  vff = Vff()
+  vff["In", "As"] = 2.62332, 21.6739, -112.0, 150.0
+  vff["Ga", "As"] = 2.44795, 32.1530, -105.0, 150.0
+  vff["As", "Ga", "As"] = "tet", -4.099, 9.3703
+  vff["Ga", "As", "Ga"] = "tet", -4.099, 9.3703
+  vff["In", "As", "In"] = "tet", -5.753, 5.7599
+  vff["As", "In", "As"] = "tet", -5.753, 5.7599
+  vff["Ga", "As", "In"] = -0.35016, -4.926, 7.5651
+
+  return vff
+
+def crystal():
+  from lada.crystal import Structure
+
+  structure = Structure( 10.0, 0.5, 0.5, 
+                         0.00, 0.0, 0.5,
+                         0.00, 0.5, 0.0, scale=6.5 )\
+                       .add_atom(pos=(0.00, 0.00, 0.00), type="Ga") \
+                       .add_atom(pos=(0.25, 0.25, 0.25), type="As") \
+                       .add_atom(pos=(1.00, 0.00, 0.00), type="Ga") \
+                       .add_atom(pos=(1.25, 0.25, 0.25), type="As") \
+                       .add_atom(pos=(2.00, 0.00, 0.00), type="In") \
+                       .add_atom(pos=(2.25, 0.25, 0.25), type="As") \
+                       .add_atom(pos=(3.00, 0.00, 0.00), type="In") \
+                       .add_atom(pos=(3.25, 0.25, 0.25), type="As") \
+                       .add_atom(pos=(4.00, 0.00, 0.00), type="Ga") \
+                       .add_atom(pos=(4.25, 0.25, 0.25), type="As") \
+                       .add_atom(pos=(5.00, 0.00, 0.00), type="In") \
+                       .add_atom(pos=(5.25, 0.25, 0.25), type="As") \
+                       .add_atom(pos=(6.00, 0.00, 0.00), type="In") \
+                       .add_atom(pos=(6.25, 0.25, 0.25), type="As") \
+                       .add_atom(pos=(7.00, 0.00, 0.00), type="Ga") \
+                       .add_atom(pos=(7.25, 0.25, 0.25), type="As") \
+                       .add_atom(pos=(8.00, 0.00, 0.00), type="Ga") \
+                       .add_atom(pos=(8.25, 0.25, 0.25), type="As") \
+                       .add_atom(pos=(9.00, 0.00, 0.00), type="Ga") \
+                       .add_atom(pos=(9.25, 0.25, 0.25), type="As")  
+  return structure
+
+def test_relaxall():
+  """ Tests gradients as implemented in the functional. """
+  from numpy import abs
+  from lada.vff.functional import Functional
+
+  vff = functional()
+  structure = crystal()
+  epsilon = 1e-5
+  
+  # gets functions for current structure
+  x, jacobian, energy, make_structure                                          \
+       = Functional(copy=vff)._getfuncs_relaxall(structure)
+  stress, forces = vff.jacobian(structure)
+
+  jac0 = jacobian(x)
+  for i in xrange(6):
+    xminus = x.copy()
+    xminus[i] = xminus[i] - epsilon * 0.5
+    eminus = energy(xminus)
+    xplus = x.copy()
+    xplus[i] = xplus[i] + epsilon * 0.5
+    eplus = energy(xplus)
+
+    assert abs(jac0[i] - (eplus - eminus) / epsilon ) < 1e-6
+
+  epsilon = 1e-7
+  for i in xrange(6, len(x)):
+    xminus = x.copy()
+    xminus[i] = xminus[i] - epsilon * 0.5
+    eminus = energy(xminus)
+    xplus = x.copy()
+    xplus[i] = xplus[i] + epsilon * 0.5
+    eplus = energy(xplus)
+
+    assert abs(jac0[i] - (eplus - eminus) / epsilon ) < 1e-6
+
+if __name__ == '__main__':
+  test_relaxall()
