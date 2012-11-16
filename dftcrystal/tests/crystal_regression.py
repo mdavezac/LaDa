8a279c06d19ae4a099cb3c1aaaf9a01f1a844cb4
Mayeul d'Avezac <m.davezac@imperial.ac.uk>
2012-11-16 14:17:04 +0000

    Correcting for bug in CRYSTAL.
      CRYSTAL fails when ATOMSYMM is called twice. At least for some
    compilers. LaDa issues warning when more than one ATOMSYMM is found and
    prints only the last.

---
+++ dftcrystal/tests/crystal_regression.py
@@ -0,0 +1,28 @@
+def test_doubleatomsymm():
+  """ Bugs when calling atomsymm twice. """
+  from tempfile import mkdtemp
+  from shutil import rmtree
+  from os.path import exists
+  from lada.dftcrystal import Crystal
+  from lada.misc import Changedir
+
+
+  c = Crystal(136, 4.63909875, 2.97938395, 
+              ifhr=0, 
+              shift=0)                                  \
+             .add_atom(0, 0, 0, 'Ti')                   \
+             .add_atom(0.306153, 0.306153, 0, 'O')      \
+             .append('ATOMSYMM')
+  
+  directory = '/tmp/test' #mkdtemp()
+  if directory == '/tmp/test':
+    if exists(directory): rmtree(directory)
+    with Changedir(directory) as cwd: pass
+  try: 
+      c.append('ATOMSYMM')
+      c.eval()
+  finally:
+    if directory != '/tmp/test': rmtree(directory)
+
+if __name__ == '__main__':
+  test_doubleatomsymm()
