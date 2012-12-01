fd31718293a93f8f41017ed6ff708bb9d371aa01
Mayeul d'Avezac <m.davezac@imperial.ac.uk>
2012-12-01 14:09:31 +0000

    added epitaxial relaxation to VFF

--- vff/functional.py
+++ vff/functional.py
@@ -6,7 +6,8 @@ class Functional(Vff):
   Extract = ExtractVFF
   """ Extraction object for Vff. """
   def __init__( self, relax=True, method='BFGS', tol=1e-8, maxiter=100,
-                verbose=True, copy=None, cartesian=True, options=None ): 
+                verbose=True, copy=None, cartesian=True, options=None,
+                direction=None ): 
     super(Functional, self).__init__()
 
     self._parameters = {}
@@ -32,6 +33,13 @@ class Functional(Vff):
     """ Additional options for the chosen minimizer. """
     self.cartesian = cartesian
     """ Whether to relax as cartesian or fractional. """
+
+    self.direction = direction
+    """ Epitaxial relaxation direction, if any. 
+
+        This should be None (no epitaxial relaxation) or a 3d-vector. If the
+        latter, than the cellshape is relaxed only in the epitaxial direction. 
+    """
 
     if copy is not None: self.__dict__.update(copy.__dict__)
 
@@ -72,7 +80,8 @@ class Functional(Vff):
       if self._is_static(**kwargs):
         result = super(Functional, self).__call__(structure)
       else: 
-        funcs = self._getfuncs_relaxall(structure)
+        funcs = self._getfuncs_relaxall(structure) if self.direction is None   \
+                else self._getfuncs_epi(structure)
         options = {} if self.options is None else self.options.copy()
         options['disp'] = self.verbose
         options['maxiter'] = self.maxiter
@@ -162,6 +171,59 @@ class Functional(Vff):
     Functions = namedtuple('Functions', ['x0', 'jacobian', 'energy', 'structure'])
     return Functions(x, jacobian, energy, make_structure)
 
+  def _getfuncs_epi(self, structure):
+    """ Functions when relaxing all degrees of freedom. """
+    from collections import namedtuple
+    from numpy import dot, array, zeros, outer, identity
+    from numpy.linalg import inv, det, norm
+    from quantities import angstrom
+    from . import build_tree
+
+    structure = structure.copy()
+    cell0, pos0 = structure.cell.copy(), structure[0].pos
+    tree = build_tree(structure)
+    scale = float(structure.scale.rescale(angstrom))
+    direction = array(self.direction, dtype='float64') / norm(self.direction)
+    strain_template = outer(direction, direction)
+
+    def update_structure(x0, strain):
+      structure.cell = dot(strain, cell0)
+      structure[0].pos = dot(strain, pos0)
+      positions = dot( strain if self.cartesian else structure.cell, 
+                       x0.reshape(-1, 3).T ).T
+      for atom, pos in zip(structure[1:], positions): atom.pos = pos
+
+    def make_structure(x0):
+      strain = strain_template * x0[0] + identity(3)
+      update_structure(x0[1:], strain)
+      return structure
+
+    def energy(x0):
+      strain = strain_template * x0[0] + identity(3)
+      update_structure(x0[1:], strain)
+      return self.energy(structure, _tree=tree).magnitude
+
+    def jacobian(x0):
+      strain = strain_template * x0[0] + identity(3)
+      update_structure(x0[1:], strain)
+
+      stress, forces = self.jacobian(structure, _tree=tree)
+      stress *= -det(structure.scale * structure.cell)
+
+      stress = dot(stress, inv(strain))
+      result = zeros(len(structure)*3-2, dtype='float64')
+      result[0] = dot(dot(direction, stress), direction)
+      result[1:] = dot( (strain if self.cartesian else structure.cell.T)*scale,
+                        forces[1:].T ).T.flatten()
+      return result
+
+    x = zeros(len(structure)*3-2, dtype='float64')
+    for i, atom in enumerate(structure[1:]): x[1+3*i:4+3*i] = atom.pos
+    if not self.cartesian:
+      x[1:] = dot(inv(structure.cell), x[1:].reshape(-1, 3).T).T.flatten()
+
+    Functions = namedtuple('Functions', ['x0', 'jacobian', 'energy', 'structure'])
+    return Functions(x, jacobian, energy, make_structure)
 del stateless
 del assign_attributes
 del Vff
---
+++ vff/tests/epirelax.py
@@ -0,0 +1,81 @@
+def functional():
+  from lada.vff.functional import Functional
+  vff = Functional()
+  vff["Si", "Si"] = 2.35905923, 45.375785351, -150.363195485, 519.230157133
+  vff["Ge", "Ge"] = 2.44167023, 37.816422666, -124.830189020, 250.179861133
+  vff["Si", "Ge"] = 2.39642606, 41.875535703, -145.994430120, 366.853558523
+  vff["Si", "Si", "Si"] = "tet", 0, 12.579328566, 44.930324684, 359.422663897
+  vff["Ge", "Ge", "Ge"] = "tet", 0, 10.383093493, 55.677487481
+  vff["Si", "Ge", "Si"] = "tet", 0, 11.902809727, 20.193404352
+  vff["Ge", "Si", "Ge"] = "tet", 0, 11.902809727, 20.193404352
+  vff["Si", "Si", "Ge"] = "tet", 0, 12.245971457, 32.561864518, 179.706525419
+  vff["Si", "Ge", "Ge"] = "tet", 0, 11.147853920, 37.930591733
+
+  return vff
+
+def crystal():
+  from lada.crystal import Structure
+
+  structure = Structure( 10.0, 0.5, 0.5, 
+                         0.00, 0.0, 0.5,
+                         0.00, 0.5, 0.0, scale=5.5 )\
+                       .add_atom(pos=(0.00, 0.00, 0.00), type="Ge") \
+                       .add_atom(pos=(0.25, 0.25, 0.25), type="Ge") \
+                       .add_atom(pos=(1.00, 0.00, 0.00), type="Ge") \
+                       .add_atom(pos=(1.25, 0.25, 0.25), type="Si") \
+                       .add_atom(pos=(2.00, 0.00, 0.00), type="Si") \
+                       .add_atom(pos=(2.25, 0.25, 0.25), type="Si") \
+                       .add_atom(pos=(3.00, 0.00, 0.00), type="Si") \
+                       .add_atom(pos=(3.25, 0.25, 0.25), type="Si") \
+                       .add_atom(pos=(4.00, 0.00, 0.00), type="Ge") \
+                       .add_atom(pos=(4.25, 0.25, 0.25), type="Si") \
+                       .add_atom(pos=(5.00, 0.00, 0.00), type="Si") \
+                       .add_atom(pos=(5.25, 0.25, 0.25), type="Si") \
+                       .add_atom(pos=(6.00, 0.00, 0.00), type="Ge") \
+                       .add_atom(pos=(6.25, 0.25, 0.25), type="Si") \
+                       .add_atom(pos=(7.00, 0.00, 0.00), type="Ge") \
+                       .add_atom(pos=(7.25, 0.25, 0.25), type="Si") \
+                       .add_atom(pos=(8.00, 0.00, 0.00), type="Ge") \
+                       .add_atom(pos=(8.25, 0.25, 0.25), type="Si") \
+                       .add_atom(pos=(9.00, 0.00, 0.00), type="Ge") \
+                       .add_atom(pos=(9.25, 0.25, 0.25), type="Si")  
+  return structure
+
+
+def test_relaxepi(direction, cartesian=True, scale=5.45):
+  from tempfile import mkdtemp
+  from os.path import exists
+  from shutil import rmtree
+  from numpy import array, outer, abs, all, dot, cross, max
+  from numpy.linalg import norm
+  from lada.misc import Changedir
+
+  direction = array(direction) / norm(direction)
+  vff = functional()
+  vff.cartesian = cartesian
+  vff.direction = direction
+  structure = crystal()
+  structure.scale = scale
+  template = outer(direction, direction)
+
+  def rmdir(cell): return cell - dot(template.T, dot(cell, template))
+  
+  directory = '/tmp/test/' # mkdtmp()
+  if directory == '/tmp/test/':
+    if exists(directory): rmtree(directory)
+    with Changedir(directory) as cwd: pass
+  try: 
+    out = vff(structure, outdir=directory, overwrite=True, verbose=True)
+    assert out.success
+    diff = out.structure.cell - out.input_structure.cell
+    maxi = max(abs(diff))
+    assert all(abs(cross(direction, diff[:, 0])) < maxi * 1e-3)
+    assert all(abs(cross(direction, diff[:, 1])) < maxi * 1e-3)
+    assert all(abs(cross(direction, diff[:, 2])) < maxi * 1e-3)
+  finally:
+    if directory != '/tmp/test/': rmtree(directory)
+
+if __name__ == '__main__':
+  test_relaxepi([0, 0, 1], True, 5.35)
+  test_relaxepi([0, 1, 1], True, 5.35)
+  test_relaxepi([1, 1, 1], True, 5.35)
