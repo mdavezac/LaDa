a01667ba6b661719cc0a79c818f8f410e30733fb
Mayeul d'Avezac <m.davezac@imperial.ac.uk>
2012-12-19 17:49:14 +0000

    Adding basic gulp interface.
      * Added some general functions to make wrapping functionals easier and
        more systematic.
      * lead to some bug corrections in dftcrystal.
      * current gulp interface tested for some things but has not yet been
        runned.

---
+++ gulp/keywords.py
@@ -0,0 +1,101 @@
+""" Holds definition of GULP specific input classes. """
+__docformat__ = "restructuredtext en"
+__all__ = ['OptiKeyword', 'TwoBody'] 
+from collections import MutableMapping
+from ..tools.input.keywords import BoolKeyword, BaseKeyword
+
+class OptiKeyword(BoolKeyword):
+  """ Keywords that depends upon opti for printing. """
+  def output_map(self, **kwargs):
+    """ Only prints if opti is on. """
+    if kwargs['gulp'].opti:
+      return super(OptiKeyword, self).output_map(**kwargs)
+
+class TwoBody(MutableMapping, BaseKeyword):
+  """ Defines parameters for a two-body GULP functional. """
+  def __init__(self, enabled=False, **kwargs):
+    """ Creates the two-body functional. """
+    super(TwoBody, self).__init__()
+    self.params = {}
+    """ Holds functional parameters. """
+    self.enabled = enabled
+    """ Whether this interaction is enabled or not. """
+    
+    # sets additional parameters to True or False
+    for key, value in kwargs.iteritems(): setattr(self, key, value == True)
+
+  def _regex_key(self, key):
+    """ Returns regex-formatted value of a key. """
+    from re import compile
+    from ..error import IndexError
+    regex = compile('([A-Z][a-z]?\d*)\s*\|?\s*(core|shell|)')
+    groups = regex.match(key).groups()
+    if groups is None:
+      raise IndexError('Could not make sense of atom {0}.'.format(key))
+    return groups[0] + '|' + ('core' if len(groups[1]) == 0 else groups[1])
+  def _key(self, key):
+    """ Creates a key from a two-body parameter. """
+    if isinstance(key, str): return key
+    if len(key) != 2: raise IndexError('Expected two atoms on input.')
+    key = self._regex_key(key[0]), self._regex_key(key[1])
+    return (key[1] + '-' + key[0]) if key[0] > key[1]                          \
+           else (key[0] + '-' + key[1])
+  def __getitem__(self, key):
+    """ Retrieves two-body item. """
+    return self.params[self._key(key)]
+  def __setitem__(self, key, value):
+    self.params[self._key(key)] = value
+  def __delitem__(self, key, value):
+    """ Deletes two-body interaction. """
+    del self.params[self._key(key)] 
+  def __len__(self):
+    """ Number of independant two-body interactions. """
+    return len(self.params)
+  def __iter__(self):
+    """ Iterates over the two-body interactions. """
+    for k in self.params: yield k.split('-')
+
+  def output_map(self, **kwargs):
+    """ Returns output map. """
+    # whether to print this or not.
+    if self.enabled == False: return 
+    # list of structures. 
+    species = None
+    if kwargs.get('structure', None) is not None:
+      species = set([u.type for u in kwargs['structure']])
+    # header is the keyword + additional parameters.
+    header = self.keyword
+    for key, value in self.__dict__.iteritems():
+      if key[0] == '_' or key in ['params', 'keyword', 'enabled'] or not value:
+        continue
+      header += ' ' + str(key) 
+    # creates list of interactions.
+    result = ""
+    for key, value in self.params.iteritems():
+      # if structure in input, check that interaction is needed.
+      if species is not None:
+        atom0, atom1 = key.split('-')
+        atom0, atom1 = atom0.split('|')[0], atom1.split('|')[0]
+        if atom0 not in species or atom1 not in species: continue
+      # print interaction.
+      result += key.replace('-', '   ').replace('|', ' ')  + '     '
+      if hasattr(value, '__iter__'):
+        result += ' '.join(str(float(u)) for u in value)
+      else: result += str(float(value))
+      result += '\n'
+    return {header: result}
+
+  def __ui_repr__(self, imports, name=None, defaults=None, exclude=None):
+    """ User-friendly representation of the functional. """
+    from ..tools.uirepr import template_ui_repr
+    if exclude is None: exclude = ['params']
+    else: exclude.append('params')
+
+    results = template_ui_repr(self, imports, name, defaults, exclude)
+    if defaults is None or self.enabled == True or len(self.params):
+      results['{0}.enabled'.format(name)] = repr(self.enabled)
+    for key, value in self.params.iteritems():
+      index = ', '.join(repr(u) for u in key.split('-'))
+      key = "{0}[{1}]".format(name, index)
+      results[key] = ', '.join(str(u) for u in value)
+    return results
---
+++ gulp/tests/extract_structure.py
@@ -0,0 +1,75 @@
+def test_noopti_anatase(path):
+  """ Test structure extraction. """
+  from os.path import join
+  from numpy import array, abs, all
+  from quantities import angstrom
+  from lada.gulp import Extract
+
+  extract = Extract(join(path, 'noopti.gout'))
+  assert not extract.optimize
+  assert not extract.conp
+  assert extract.qeq
+  cell = array([[-1.897114,  1.897114,  4.884209],
+                [ 1.897114, -1.897114,  4.884209],
+                [ 1.897114,  1.897114, -4.884209]]) * angstrom
+  assert all(abs(extract.cell - cell) < 1e-4)
+  assert extract.cell is extract.input_cell
+  assert all(abs(extract.structure.cell - cell.magnitude) < 1e-4)
+  assert len(extract.structure) == 2
+  assert extract.structure[0].type == 'Ti'
+  assert all(abs(extract.structure[0].pos) < 1e-4)
+  assert extract.structure[1].type == 'O'
+  assert all(abs(extract.structure[1].pos - [0, 0, 0.20474]) < 1e-4)
+  assert all(abs(extract.structure[1].pos - [0, 0, 0.20474]) < 1e-4)
+  assert extract.structure is extract.input_structure
+  assert getattr(extract.structure[0], 'label', 0) == 1
+  assert getattr(extract.structure[1], 'label', 0) == 2
+  assert abs(getattr(extract.structure[0], 'occupancy', 0) - 1e0) < 1e-8
+  assert abs(getattr(extract.structure[1], 'occupancy', 0) - 1e0) < 1e-8
+
+
+def test_opti_anatase(path):
+  """ Test structure optimization extraction. """
+  from os.path import join
+  from numpy import array, abs, all
+  from quantities import angstrom, e, eV
+  from lada.gulp import Extract
+
+  extract = Extract(join(path, 'opti.gout'))
+  assert extract.optimize
+  assert extract.conp
+  assert extract.qeq
+  cell = array([[-1.897114,  1.897114,  4.884209],
+                [ 1.897114, -1.897114,  4.884209],
+                [ 1.897114,  1.897114, -4.884209]]) * angstrom
+  assert all(abs(extract.input_cell - cell) < 1e-4)
+  assert extract.cell is not extract.input_cell
+  assert all(abs(extract.input_structure.cell - cell.magnitude) < 1e-4)
+  assert len(extract.input_structure) == 2
+  assert extract.input_structure[0].type == 'Ti'
+  assert all(abs(extract.input_structure[0].pos) < 1e-4)
+  assert extract.input_structure[1].type == 'O'
+  assert all(abs(extract.input_structure[1].pos - [0, 0, 0.20474]) < 1e-4)
+  assert all(abs(extract.input_structure[1].pos - [0, 0, 0.20474]) < 1e-4)
+  assert extract.structure is not extract.input_structure
+
+  cell = array([[-1.924847,  1.924847,  4.53005 ],
+                [ 1.924847, -1.924847,  4.53005 ],
+                [ 1.924847,  1.924847, -4.53005 ]]) * angstrom
+  assert all(abs(extract.cell - cell) < 1e-4)
+  assert len(extract.structure) == 2
+  assert extract.structure[0].type == 'Ti'
+  assert all(abs(extract.structure[0].pos) < 1e-4)
+  assert extract.structure[1].type == 'O'
+  assert all(abs(extract.structure[1].pos - [0, 0, 0.216931]) < 1e-4)
+  assert getattr(extract.structure[0], 'label', 0) == 1
+  assert getattr(extract.structure[1], 'label', 0) == 2
+  assert abs(getattr(extract.structure[0], 'charge', 0) - 1.131064*e) < 1e-5
+  assert abs(getattr(extract.structure[1], 'charge', 0) + 0.565532*e) < 1e-5
+  assert abs(extract.structure.energy + 16.19739956 * eV) < 1e-5
+
+if __name__ == '__main__':
+  from sys import argv
+  from os.path import dirname, join
+  test_noopti_anatase(join(dirname(argv[0]), 'data'))
+  test_opti_anatase(join(dirname(argv[0]), 'data'))
